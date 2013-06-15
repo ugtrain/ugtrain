8899b379cc1aebadca4919534a7e118ec09f5f69
Sebastian Riemer <sebastian.riemer@gmx.de>
2013-06-15 15:25:52 +0200

    ugpreload: simplify LD_PRELOAD handling w. setenv()

--- linuxhooking/ugpreload.c
+++ linuxhooking/ugpreload.c
@@ -50,55 +50,42 @@ void print_env (char **envp)
 }
 #endif
 
-/*
- * can't rely on "envz_add" or "setenv" - these crash
- *
- * This function removes an old occurence of the requested
- * env var, appends the requested env value and returns the
- * new env length.
- *
- * You need to preallocate the additionally needed memory,
- * you need to know the envp length, the string count and
- * where to remove the env value to be added if it is
- * already set.
- */
-int my_env_add (char **envp, size_t *env_len, int count,
-		const char *env_entry, int rpos, size_t rlen)
+int env_append(const char *name, const char *val, char separator)
 {
-	int i;
+	size_t env_len;
+	const char *old_env;
+	char *new_env;
 
-	if (rlen > 0) {
-		/* move memory to overwrite the old value
-		   and update pointers */
-		memmove(envp[rpos], envp[rpos] + rlen,
-			*env_len - (envp[rpos] - envp[0]) - rlen);
-		for (i = rpos + 1; i <= count; i++)
-			envp[i-1] = envp[i] - rlen;
-		i = count - 1;
-		envp[i+1] = NULL;
+	old_env = getenv(name);
+
+	if (old_env != NULL) {
+		env_len = strlen(old_env) + strlen(val) + 2;
+		new_env = malloc(env_len);
+
+		memcpy(new_env, old_env, strlen(old_env));
+		memcpy(&new_env[strlen(old_env) + 1], val, strlen(val));
+
+		new_env[strlen(old_env)] = separator;
+		new_env[env_len - 1] = '\0';
 	} else {
-		rlen = 0;
-		i = count;
+		env_len = strlen(val) + 1;
+		new_env = malloc(env_len);
+
+		memcpy(new_env, val, env_len - 1);
+		new_env[env_len - 1] = '\0';
 	}
 
-	/* append the new env value */
-	memcpy(envp[i], env_entry, strlen(env_entry) + 1);
-
-	/* update the env length */
-	*env_len += strlen(env_entry) + 1 - rlen;
+	setenv(name, new_env, 1);
+	free(new_env);
 
 	return 0;
 }
 
-int main (int argc, char *argv[], char *envp[])
+int main (int argc, char *argv[])
 {
-	char preload_str[PATH_MAX] = PRELOAD_VAR "=";
-	u32 preload_str_size = 0;
 	char *game_path = NULL;
-	int i, rpos = 0, count = 0, status;
+	int status;
 	pid_t pid;
-	size_t rlen = 0, env_len = 0, new_len = 0, str_len = 0, str_start = 0;
-	char **new_envp = NULL;
 
 	if (argc < 3) {
 		fprintf(stderr, "use the following parameters: "
@@ -106,57 +93,17 @@ int main (int argc, char *argv[], char *envp[])
 		return 1;
 	}
 
-	preload_str_size = sizeof(PRELOAD_VAR "=") + strlen(argv[1]);
-	if (preload_str_size > PATH_MAX) {
-		fprintf(stderr, "The lib_path is too long.\n");
-		return 1;
-	}
-	strcat(preload_str, argv[1]);
 	game_path = argv[2];
 
-	/* get length of env and check if env var
-	   to be added must be overwritten */
-	for (i = 0; envp[i] != NULL; i++) {
-		env_len += strlen(envp[i]) + 1;
-		if (!strncmp(envp[i], PRELOAD_VAR, strlen(PRELOAD_VAR))) {
-			rpos = i;
-			rlen = strlen(envp[i]) + 1;
-		}
-	}
-	count = i;
-
-	/* prepare heap memory for string pointers followed by strings,
-	   add the needed space to insert our LD_PRELOAD string */
-	str_start = (count+2) * sizeof(char *);
-	new_len = str_start + env_len + preload_str_size;
-	new_envp = (char **) malloc(new_len);
-	if (!new_envp) {
-		perror("malloc");
-		return 1;
-	}
-
-	/* set string pointer first, then copy env string from stack
-	   to bigger heap for data insertion */
-	for (i = 0; i < count; i++) {
-		new_envp[i] = PTR_ADD(char *, &new_envp[0],
-				      str_start + str_len);
-		strcpy(new_envp[i], envp[i]);
-		str_len += strlen(envp[i]) + 1;
-	}
-	new_envp[i] = PTR_ADD(char *, &new_envp[0],
-			      str_start + str_len);   /* for string to be inserted */
-	new_envp[i+1] = NULL; /* end of env */
-
-	/* add the new env val to the array */
-	my_env_add(new_envp, &env_len, count, preload_str, rpos, rlen);
-
+	/* append the library to the preload env var */
+	env_append(PRELOAD_VAR, argv[1], ':');
 
 	/* run the victim process */
 	pid = fork();
 	if (pid == 0) {
 		printf("fork worked\n");
-		if (execle(game_path, game_path, NULL, new_envp) < 0) {
-			perror("execle");
+		if (execl(game_path, game_path, NULL) < 0) {
+			perror("execl");
 			return 1;
 		}
 	} else if (pid > 0) {
@@ -165,9 +112,6 @@ int main (int argc, char *argv[], char *envp[])
 	} else {
 		perror("fork");
 	}
-
-	if (new_envp)
-		free(new_envp);
 
 	return EXIT_SUCCESS;
 }
