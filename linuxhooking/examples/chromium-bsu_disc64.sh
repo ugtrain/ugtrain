c8ac8a898267f2d45da1b6b2717e4e0fea9f9df3
Sebastian Riemer <sebastian.riemer@gmx.de>
2013-07-10 10:01:02 +0200

    chromium-bsu_disc64.sh: update heap filtering

--- linuxhooking/examples/chromium-bsu_disc64.sh
+++ linuxhooking/examples/chromium-bsu_disc64.sh
@@ -17,6 +17,7 @@
 
 # The offset on the stack is relative to the stack end as the stack end isn't
 # a static value between multiple application runs.
+# The same applies for the heap filter as Linux likely uses heap randomization.
 
 CWD=`dirname $0`
 cd "$CWD"
@@ -30,14 +31,14 @@ mkfifo -m 0666 /tmp/memhack_out > /dev/null 2>&1
 
 # set config for 64-bit chromium-bsu 0.9.15-1.2 (OpenSuse 11.4)
 # Format: "1 char stage number (1..4);"
-# Stage1: "heap addr min; heap addr max;"  <-- use 0x0 on both to get all mallocs (and frees)
+# Stage1: "heap offs min; heap offs max;"  <-- use 0x0 on both to get all mallocs (and frees)
 # Stage2: "malloc size;"
 # Stage3: "code addr min; code addr max;" (from .text for backtracing)
 # Stage4: "[unique code addr of the interesting malloc]" <-- optional
 #bash -c 'echo "1;0x0;0x0" > /tmp/memhack_in; cat /tmp/memhack_out > `dirname $0`/memhack_file' &
-#bash -c 'echo "2;0x0;0x0;288" > /tmp/memhack_in; cat /tmp/memhack_out' &
-#bash -c 'echo "3;0x7b0000;0x850000;288;0x404d50;0x425c88" > /tmp/memhack_in; cat /tmp/memhack_out' &
-bash -c 'echo "4;0x0;0x0;288;0x404d50;0x425c88;0x411097" > /tmp/memhack_in; cat /tmp/memhack_out' &
+#bash -c 'echo "2;0x0;0x350000;288" > /tmp/memhack_in; cat /tmp/memhack_out' &
+#bash -c 'echo "3;0x0;0x350000;288;0x404d50;0x425c88" > /tmp/memhack_in; cat /tmp/memhack_out' &
+bash -c 'echo "4;0x0;0x350000;288;0x404d50;0x425c88;0x411097" > /tmp/memhack_in; cat /tmp/memhack_out' &
 
 # discovered stack offset:     0x198    (after stage 4)
 # discovered offset in object: 0xbc     (LIVES)
