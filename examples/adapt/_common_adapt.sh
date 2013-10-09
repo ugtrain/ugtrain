736c34db6293a9851533c0b3e609b0dab1696286
Sebastian Riemer <sebastian.riemer@gmx.de>
2013-10-09 09:57:59 +0200

    adapt: common: prefer unique matches

--- examples/adapt/_common_adapt.sh
+++ examples/adapt/_common_adapt.sh
@@ -13,6 +13,7 @@ function get_malloc_code_3()
     msize="$3"
     explines=$4
     taillines=$5
+    isunique=0
 
     IFS=`printf '\n+'`
     if [ $DEBUG -eq 1 ]; then echo "objdump -D $app_path | grep $fname -B 2 -A 1 | grep -A 2 $msize"; fi
@@ -22,13 +23,19 @@ function get_malloc_code_3()
     if [ $DEBUG -eq 1 ]; then echo -e "$CODE_PART"; fi
 
     CODE_LINES=`echo -e "$CODE_PART" | wc -l`
-    if [ $CODE_LINES -ne $explines ]; then exit 1; fi
+    if [ $CODE_LINES -eq 3 ]; then
+        isunique=1
+    elif [ $CODE_LINES -ne $explines ]; then
+	exit 1
+    fi
 
-    CODE_PART=`echo -e "$CODE_PART" | tail -n $taillines`
-    CODE_PART=`echo -e "$CODE_PART" | head -n 3`
+    if [ $isunique -ne 1 ]; then
+        CODE_PART=`echo -e "$CODE_PART" | tail -n $taillines`
+        CODE_PART=`echo -e "$CODE_PART" | head -n 3`
 
-    CODE_LINES=`echo -e "$CODE_PART" | wc -l`
-    if [ $CODE_LINES -ne 3 ]; then exit 1; fi
+        CODE_LINES=`echo -e "$CODE_PART" | wc -l`
+        if [ $CODE_LINES -ne 3 ]; then exit 1; fi
+    fi
 
     CODE_CALL=`echo -e "$CODE_PART" | cut -d '
 ' -f 2 | grep call`
@@ -47,6 +54,7 @@ function get_malloc_code_4()
     msize="$3"
     explines=$4
     taillines=$5
+    isunique=0
 
     IFS=`printf '\n+'`
     if [ $DEBUG -eq 1 ]; then echo "objdump -D $app_path | grep $fname -B 2 -A 1 | grep -A 3 $msize"; fi
@@ -56,13 +64,19 @@ function get_malloc_code_4()
     if [ $DEBUG -eq 1 ]; then echo -e "$CODE_PART"; fi
 
     CODE_LINES=`echo -e "$CODE_PART" | wc -l`
-    if [ $CODE_LINES -ne $explines ]; then exit 1; fi
+    if [ $CODE_LINES -eq 4 ]; then
+        isunique=1
+    elif [ $CODE_LINES -ne $explines ]; then
+        exit 1
+    fi
 
-    CODE_PART=`echo -e "$CODE_PART" | tail -n $taillines`
-    CODE_PART=`echo -e "$CODE_PART" | head -n 4`
+    if [ $isunique -ne 1 ]; then
+        CODE_PART=`echo -e "$CODE_PART" | tail -n $taillines`
+        CODE_PART=`echo -e "$CODE_PART" | head -n 4`
 
-    CODE_LINES=`echo -e "$CODE_PART" | wc -l`
-    if [ $CODE_LINES -ne 4 ]; then exit 1; fi
+        CODE_LINES=`echo -e "$CODE_PART" | wc -l`
+        if [ $CODE_LINES -ne 4 ]; then exit 1; fi
+    fi
 
     CODE_CALL=`echo -e "$CODE_PART" | cut -d '
 ' -f 3 | grep call`
