ugtrain/ugtrain

commit e11f22ea2f9c1e585c9359f5369666a4584ea123

ugtrain: add 'game_path' config command
Now, we can specify an absolute path to the game. This is then used
to run the game and for 'objdump' in the discovery as well as in the
adaption scripts.

src/ugtrain.cpp
@@ -465,13 +465,13 @@ static void change_memory (pid_t pid, CfgEntry *cfg_en, u8 *buf,
 	}
 }
 
-static i32 run_game (char *proc_name)
+static i32 run_game (char *game_path)
 {
 	i32 ret;
-	const char *cmd = (const char *) proc_name;
+	const char *cmd = (const char *) game_path;
 	char *cmdv[2];
 
-	cmdv[0] = proc_name;
+	cmdv[0] = game_path;
 	cmdv[1] = NULL;
 
 	cout << "$ " << cmdv[0] << " &" << endl;
@@ -487,7 +487,7 @@ err:
 }
 
 #ifdef __linux__
-static i32 run_preloader (char *preload_lib, char *proc_name)
+static i32 run_preloader (char *preload_lib, char *game_path)
 {
 	i32 ret;
 	const char *cmd = (const char *) PRELOADER;
@@ -495,7 +495,7 @@ static i32 run_preloader (char *preload_lib, char *proc_name)
 
 	cmdv[0] = (char *) PRELOADER;
 	cmdv[1] = preload_lib;
-	cmdv[2] = proc_name;
+	cmdv[2] = game_path;
 	cmdv[3] = NULL;
 
 	cout << "$ " << cmdv[0] << " " << cmdv[1]
@@ -611,7 +611,7 @@ skip_memhack:
 	/* Run the preloaded game but not as root */
 	if (opt->preload_lib && getuid() != 0) {
 		cout << "Starting preloaded game.." << endl;
-		run_preloader(opt->preload_lib, opt->proc_name);
+		run_preloader(opt->preload_lib, opt->game_path);
 	}
 
 	cout << "Waiting for preloaded game.." << endl;
@@ -693,13 +693,15 @@ parse_err:
 	return -1;
 }
 
-static i32 adapt_config (list<CfgEntry> *cfg, char *adp_script)
+static i32 adapt_config (list<CfgEntry> *cfg, char *adp_script,
+			 char *game_path)
 {
 	char pbuf[PIPE_BUF] = { 0 };
 	ssize_t read_bytes;
 	const char *cmd = (const char *) adp_script;
 	char *cmdv[] = {
 		adp_script,
+		game_path,
 		NULL
 	};
 
@@ -857,6 +859,11 @@ i32 main (i32 argc, char **argv, char **env)
 	    (opt.disc_str[0] >= '0' && opt.disc_str[0] <= '4'))
 		emptycfg = 1;
 
+	if (!opt.game_path)
+		opt.game_path = get_abs_app_path(opt.proc_name);
+	if (!opt.game_path)
+		return -1;
+
 	cout << "Config:" << endl;
 	output_config(&cfg);
 	cout << "Activated:" << endl;
@@ -892,7 +899,7 @@ i32 main (i32 argc, char **argv, char **env)
 			cerr << "Error, no adaption script!" << endl;
 			return -1;
 		}
-		if (adapt_config(&cfg, opt.adp_script) != 0) {
+		if (adapt_config(&cfg, opt.adp_script, opt.game_path) != 0) {
 			cerr << "Error while code address adaption!" << endl;
 			return -1;
 		}
@@ -917,7 +924,7 @@ prepare_dynmem:
 				return -1;
 #endif
 			cout << "Starting the game.." << endl;
-			run_game(opt.proc_name);
+			run_game(opt.game_path);
 			sleep_sec(1);
 			pid = proc_to_pid(opt.proc_name);
 			if (pid < 0)

examples/adapt/chromium-bsu_adapt32.sh
@@ -14,7 +14,7 @@
 
 CWD=`dirname $0`
 cd "$CWD"
-APP_PATH=`which chromium-bsu`
+APP_PATH="$1"
 
 IFS=`printf '\n+'`
 CODE_PART=`objdump -D "$APP_PATH" | grep "_Znwj" -B 2 -A 1 | grep -A 2 0x110`

examples/adapt/chromium-bsu_adapt64.sh
@@ -14,7 +14,7 @@
 
 CWD=`dirname $0`
 cd "$CWD"
-APP_PATH=`which chromium-bsu`
+APP_PATH="$1"
 
 IFS=`printf '\n+'`
 CODE_PART=`objdump -D "$APP_PATH" | grep "_Znwm" -B 2 -A 1 | grep -A 3 0x120`

examples/adapt/warzone2100_adapt32.sh
@@ -13,7 +13,7 @@
 
 CWD=`dirname $0`
 cd "$CWD"
-APP_PATH=`which warzone2100`
+APP_PATH="$1"
 APP_VERS=`${APP_PATH} --version | grep -o "Version.*" | cut -d ' ' -f 2`
 
 IFS=`printf '\n+'`

examples/adapt/warzone2100_adapt64.sh
@@ -14,7 +14,7 @@
 
 CWD=`dirname $0`
 cd "$CWD"
-APP_PATH=`which warzone2100`
+APP_PATH="$1"
 APP_VERS=`${APP_PATH} --version | grep -o "Version.*" | cut -d ' ' -f 2`
 
 IFS=`printf '\n+'`

src/cfgparser.cpp
@@ -35,7 +35,8 @@ enum {
 	NAME_DYNMEM_STACK,
 	NAME_DYNMEM_IGN,
 	NAME_ADAPT,
-	NAME_ADP_REQ
+	NAME_ADP_REQ,
+	NAME_GAME_PATH
 };
 
 static inline void proc_name_err (string *line, u32 lidx)
@@ -118,6 +119,11 @@ static string parse_value_name (string *line, u32 lnr, u32 *start,
 			*name_type = NAME_ADAPT;
 		else if (ret.substr(6, string::npos) == "required")
 			*name_type = NAME_ADP_REQ;
+		else
+			*name_type = NAME_REGULAR;
+	} else if (ret.substr(0, 5) == "game_") {
+		if (ret.substr(5, string::npos) == "path")
+			*name_type = NAME_GAME_PATH;
 		else
 			*name_type = NAME_REGULAR;
 	} else {
@@ -351,7 +357,7 @@ list<CfgEntry*> *read_config (string *path,
 	bool in_dynmem = false;
 	string line;
 	string home(opt->home);
-	string adp_str;
+	string tmp_str;
 	size_t pos;
 
 	// read config into string vector
@@ -436,15 +442,15 @@ list<CfgEntry*> *read_config (string *path,
 			if (in_dynmem)
 				cfg_parse_err(&line, lnr, start);
 
-			adp_str = string("");
+			tmp_str = string("");
 			pos = path->rfind("/");
 			if (pos != string::npos)
-				adp_str.append(path->substr(0, pos + 1));
-			adp_str.append(parse_value_name(&line,
+				tmp_str.append(path->substr(0, pos + 1));
+			tmp_str.append(parse_value_name(&line,
 				       lnr, &start, &name_type));
 
 			// Copy into C string
-			opt->adp_script = to_c_str(&adp_str);
+			opt->adp_script = to_c_str(&tmp_str);
 			break;
 
 		case NAME_ADP_REQ:
@@ -454,6 +460,23 @@ list<CfgEntry*> *read_config (string *path,
 			opt->adp_required = parse_value(&line, lnr, &start,
 							false, false, NULL, NULL);
 			opt->adp_req_line = lnr;
+			break;
+
+		case NAME_GAME_PATH:
+			if (in_dynmem)
+				cfg_parse_err(&line, lnr, start);
+
+			tmp_str = parse_value_name(&line,
+				  lnr, &start, &name_type);
+
+			pos = tmp_str.rfind("/");
+			if (pos != string::npos &&
+			    tmp_str.substr(pos + 1, string::npos) !=
+			    opt->proc_name)
+				cfg_parse_err(&line, lnr, start);
+
+			// Copy into C string
+			opt->game_path = to_c_str(&tmp_str);
 			break;
 
 		default:

src/discovery.cpp
@@ -189,9 +189,9 @@ static void process_disc1234_malloc (list<CfgEntry> *cfg,
 			// get the function call from disassembly
 			tmp_str = to_string(codes[i]);
 
-			cmd_str = "objdump -D `which ";
-			cmd_str += dpp->opt->proc_name;
-			cmd_str += "` | grep -B 1 -e \"^[ ]\\+";
+			cmd_str = "objdump -D ";
+			cmd_str += dpp->opt->game_path;
+			cmd_str += " | grep -B 1 -e \"^[ ]\\+";
 			cmd_str += tmp_str.substr(2, string::npos);  // no '0x'
 			cmd_str += ":\" | head -n 1 | grep -o -e \"call.*$\" "
 				   "| grep -o -e \"<.*@plt>\"";
@@ -411,9 +411,9 @@ i32 prepare_discovery (struct app_options *opt, list<CfgEntry> *cfg)
 			goto err;
 		}
 		if (ret < 5) {
-			cmd_str = "objdump -p `which ";
-			cmd_str += opt->proc_name;
-			cmd_str += "` | grep \"INIT\\|FINI\" "
+			cmd_str = "objdump -p ";
+			cmd_str += opt->game_path;
+			cmd_str += " | grep \"INIT\\|FINI\" "
 				   "| tr -d [:upper:] | tr -d [:blank:]";
 			cout << "$ " << cmd_str << endl;
 			if (run_cmd_pipe(cmd_str.c_str(), NULL, pbuf,

src/options.c
@@ -97,6 +97,7 @@ static void init_options (struct app_options *opt)
 	/* no direct CLI input */
 	opt->home = NULL;
 	opt->proc_name = NULL;
+	opt->game_path = NULL;
 	opt->adp_script = NULL;
 	opt->adp_required = 0;
 	opt->adp_req_line = 0;

commit 970727c60928bc0573f7fa523013713839e4b826

ugtrain: only check absolute path if '-P' is given
We only need the absolute game path for running the game. So
neither try to get it nor fail if it's not set with pure static
memory configs without '-P', '-D' or '-A' option given.

Fixes a regression introduced by commit e11f22ea
"ugtrain: add 'game_path' config command"

src/ugtrain.cpp
@@ -738,10 +738,12 @@ i32 main (i32 argc, char **argv, char **env)
 		allow_empty_cfg = true;
 	}
 
-	if (!opt->game_path)
-		opt->game_path = get_abs_app_path(opt->game_call);
-	if (!opt->game_path)
-		return -1;
+	if (opt->preload_lib) {
+		if (!opt->game_path)
+			opt->game_path = get_abs_app_path(opt->game_call);
+		if (!opt->game_path)
+			return -1;
+	}
 	if (!opt->game_binpath)
 		opt->game_binpath = opt->game_path;
 