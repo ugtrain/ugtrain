ugtrain/ugtrain

commit b6670cff403a5735b60e541cf8000e963f4026a0

system: introduce the rm_files_by_pattern() function
For the following commits we need a helper function to remove files
with a file name pattern. So add it by using the 'rm -f' shell
command on Linux and adding a stub for Windows. The function returns
0 if the removal worked.

src/lib/system.h
@@ -76,6 +76,16 @@ static inline i32 rm_file (const char *path)
 	return unlink(path);
 }
 
+static inline i32 rm_files_by_pattern (char *pattern)
+{
+	const char *cmd = "rm";
+	char *cmdv[4] = { (char *) "rm", (char *) "-f", pattern, NULL };
+	if (run_cmd(cmd, cmdv) < 0)
+		return -1;
+	else
+		return 0;
+}
+
 static inline void sleep_sec (u32 sec)
 {
 	sleep(sec);
@@ -130,6 +140,11 @@ static inline bool file_exists (const char *path)
 }
 
 static inline i32 rm_file (const char *path)
+{
+	return -1;
+}
+
+static inline i32 rm_files_by_pattern (char *pattern)
 {
 	return -1;
 }

commit 19505bb89a17e30ba4f68c34c79addfe44ea9e58

system: fix rm_files_by_pattern() by using a shell
Shell file patterns don't work if executing commands directly.
So rework rm_files_by_pattern() by using a shell.

Fixes: b6670cff403a ("System: introduce the rm_files_by_pattern() function")

src/lib/system.h
@@ -18,6 +18,7 @@
 #define SYSTEM_H
 
 #include <unistd.h>
+#include <stdio.h>
 #ifdef __linux__
 #include <sys/wait.h>
 #include <sys/select.h>
@@ -78,9 +79,11 @@ static inline i32 rm_file (const char *path)
 
 static inline i32 rm_files_by_pattern (char *pattern)
 {
-	const char *cmd = "rm";
-	char *cmdv[4] = { (char *) "rm", (char *) "-f", pattern, NULL };
-	if (run_cmd(cmd, cmdv) < 0)
+	char cmd_str[PIPE_BUF] = { 0 };
+
+	/* using the shell is required */
+	snprintf(cmd_str, PIPE_BUF - 1, "rm -f %s", pattern);
+	if (run_cmd(cmd_str, NULL) < 0)
 		return -1;
 	else
 		return 0;