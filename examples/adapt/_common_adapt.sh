ugtrain/ugtrain

commit 83a5456564d85ed3cb1711475351b49ca7b8abce

adapt: get rid of bashism
We call so simple functions to adapt code addresses. There is no
need for bash specific shell code.

examples/adapt/_common_adapt.sh
@@ -1,23 +1,19 @@
-#!/bin/bash
-
 # The issue is that code address and stack offsets for mallocs can differ
 # between distributions, compilers and game versions. But the thing which
 # remains constant is often the way how the code internally works.
 
+DEBUG=0
 if [ -n "$2" ]; then
-    if [ "$2" == "DEBUG" ]; then
+    if [ "$2" = "DEBUG" ]; then
         DEBUG=1
     fi
-fi
-if [ ! -v DEBUG ]; then
-    DEBUG=0
 fi
 
 CODE=""
 OLD_FNAME=""
 FUNC_CALLS=""
 
-function get_malloc_code()
+get_malloc_code()
 {
     app_path="$1"
     fname="$2"
@@ -28,12 +24,12 @@ function get_malloc_code()
     isunique=0
     RC=0
 
-    let "alines = $reslines - 1"
-    let "blines = $alines - 1"
+    alines=`expr $reslines - 1`
+    blines=`expr $alines - 1`
     if [ $blines -lt 2 ]; then blines=2; fi
 
     IFS=`printf '\n+'`
-    if [ "$CODE" == "" ]; then
+    if [ -z "$CODE" ]; then
         if [ $DEBUG -eq 1 ]; then echo "objdump -D $app_path"; fi
         CODE=`objdump -D "$app_path"`
     fi
@@ -44,7 +40,7 @@ function get_malloc_code()
     fi
     if [ $DEBUG -eq 1 ]; then echo "echo -e \$FUNC_CALLS | grep -A $alines $msize"; fi
     CODE_PART=`echo -e "$FUNC_CALLS" | grep -A $alines "$msize"`
-    if [ "$CODE_PART" == "" ]; then RC=1; return; fi
+    if [ -z "$CODE_PART" ]; then RC=1; return; fi
     if [ $DEBUG -eq 1 ]; then echo -e "$CODE_PART"; fi
 
     CODE_LINES=`echo -e "$CODE_PART" | wc -l`
@@ -64,11 +60,11 @@ function get_malloc_code()
 
     CODE_CALL=`echo -e "$CODE_PART" | cut -d '
 ' -f $alines | grep call`
-    if [ "$CODE_CALL" == "" ]; then RC=1; return; fi
+    if [ -z "$CODE_CALL" ]; then RC=1; return; fi
     if [ $DEBUG -eq 1 ]; then echo -e "CODE_CALL:\n$CODE_CALL"; fi
 
     CODE_ADDR=`echo -e "$CODE_PART" | tail -n 1 | cut -d ':' -f 1 | tr -d [:blank:]`
-    if [ "$CODE_ADDR" == "" ]; then RC=1; return; fi
+    if [ -z "$CODE_ADDR" ]; then RC=1; return; fi
     if [ $DEBUG -eq 1 ]; then echo -e "CODE_ADDR:\n$CODE_ADDR"; fi
     IFS=''
 }

examples/adapt/chromium-bsu_adapt32.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Tested with: chromium-bsu 0.9.14.1, 0.9.15, 0.9.15.1
 
@@ -14,7 +14,7 @@ APP_PATH="$1"
 RC=0
 MSIZE="0x110"
 
-. _common_adapt.sh
+. ./_common_adapt.sh
 
 get_malloc_code "$APP_PATH" "\<_Znwj@plt\>" "$MSIZE," 3 3 3
 if [ $RC -ne 0 ]; then exit 1; fi

examples/adapt/chromium-bsu_adapt64.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Tested with: chromium-bsu 0.9.14.1, 0.9.15, 0.9.15.1
 
@@ -14,7 +14,7 @@ APP_PATH="$1"
 RC=0
 MSIZE="0x120"
 
-. _common_adapt.sh
+. ./_common_adapt.sh
 
 get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE," 4 4 4
 if [ $RC -ne 0 ]; then exit 1; fi

examples/adapt/sauerbraten_adapt32.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Tested with: sauerbraten 0.0.20100728.dfsg
 
@@ -14,7 +14,7 @@ APP_PATH="$1"
 RC=0
 MSIZE1="0x560"
 
-. _common_adapt.sh
+. ./_common_adapt.sh
 
 get_malloc_code "$APP_PATH" "\<_Znwj@plt\>" "$MSIZE1," 3 11 7
 MSIZE="$MSIZE1"

examples/adapt/sauerbraten_adapt64.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Tested with: sauerbraten 0.0.20100728.dfsg, 0.0.20130203.dfsg
 
@@ -15,7 +15,7 @@ RC=0
 MSIZE1="0x598"
 MSIZE2="0x5b0"
 
-. _common_adapt.sh
+. ./_common_adapt.sh
 
 # try 0.0.20100728.dfsg
 get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE1," 5 15 11

examples/adapt/warzone2100_adapt32.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Tested with: Warzone 2100 2.3.8, 2.3.9, 3.1~rc2, 3.1.0, 3.1.1
 
@@ -16,9 +16,9 @@ MSIZE12="0x2fc"
 MSIZE21="0x128"
 MSIZE22="0x16c"
 
-. _common_adapt.sh
+. ./_common_adapt.sh
 
-if [ "$APP_VERS" == "2.3.8" -o "$APP_VERS" == "2.3.9" ]; then
+if [ "$APP_VERS" = "2.3.8" -o "$APP_VERS" = "2.3.9" ]; then
   get_malloc_code "$APP_PATH" "\<malloc@plt\>" "$MSIZE11," 3 3 3
   if [ $RC -ne 0 ]; then exit 1; fi
   MSIZE1="$MSIZE11"
@@ -32,7 +32,7 @@ CODE_ADDR1="$CODE_ADDR"
 
 ############################################
 
-if [ "$APP_VERS" == "2.3.8" -o "$APP_VERS" == "2.3.9" ]; then
+if [ "$APP_VERS" = "2.3.8" -o "$APP_VERS" = "2.3.9" ]; then
   get_malloc_code "$APP_PATH" "\<malloc@plt\>" "$MSIZE21," 3 7 7
   if [ $RC -ne 0 ]; then exit 1; fi
   MSIZE2="$MSIZE21"

examples/adapt/warzone2100_adapt64.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Tested with: Warzone 2100 2.3.8, 2.3.9, 3.1~rc2, 3.1.0, 3.1.1
 
@@ -16,9 +16,9 @@ MSIZE12="0x360"
 MSIZE21="0x160"
 MSIZE22="0x1a8"
 
-. _common_adapt.sh
+. ./_common_adapt.sh
 
-if [ "$APP_VERS" == "2.3.8" -o "$APP_VERS" == "2.3.9" ]; then
+if [ "$APP_VERS" = "2.3.8" -o "$APP_VERS" = "2.3.9" ]; then
     get_malloc_code "$APP_PATH" "\<malloc@plt\>" "$MSIZE11," 3 3 3
     if [ $RC -ne 0 ]; then exit 1; fi
     MSIZE1="$MSIZE11"
@@ -35,7 +35,7 @@ CODE_ADDR1="$CODE_ADDR"
 
 ############################################
 
-if [ "$APP_VERS" == "2.3.8" -o "$APP_VERS" == "2.3.9" ]; then
+if [ "$APP_VERS" = "2.3.8" -o "$APP_VERS" = "2.3.9" ]; then
     get_malloc_code "$APP_PATH" "\<malloc@plt\>" "$MSIZE21," 3 7 7
     if [ $RC -ne 0 ]; then exit 1; fi
     MSIZE2="$MSIZE21"

commit ffb3e58673bf56f790d35c6f333ad3a895829988

adapt: common: remove the '-e' option from echo
Using the dash on Ubuntu instead of the bash causes the '-e' option
of 'echo' to be printed. We don't need it. So remove it.
This only affects debugging output of the adaption scripts.

Fixes: 83a5456564d8 ("adapt: get rid of bashism")

examples/adapt/_common_adapt.sh
@@ -34,16 +34,16 @@ get_malloc_code()
         CODE=`objdump -D "$app_path"`
     fi
     if [ "$OLD_FNAME" != "$fname" ]; then
-        if [ $DEBUG -eq 1 ]; then echo "echo -e \$CODE | grep $fname -B $blines -A 1"; fi
-        FUNC_CALLS=`echo -e "$CODE" | grep "$fname" -B $blines -A 1`
+        if [ $DEBUG -eq 1 ]; then echo "echo \$CODE | grep $fname -B $blines -A 1"; fi
+        FUNC_CALLS=`echo "$CODE" | grep "$fname" -B $blines -A 1`
         OLD_FNAME="$fname"
     fi
-    if [ $DEBUG -eq 1 ]; then echo "echo -e \$FUNC_CALLS | grep -A $alines $msize"; fi
-    CODE_PART=`echo -e "$FUNC_CALLS" | grep -A $alines "$msize"`
+    if [ $DEBUG -eq 1 ]; then echo "echo \$FUNC_CALLS | grep -A $alines $msize"; fi
+    CODE_PART=`echo "$FUNC_CALLS" | grep -A $alines "$msize"`
     if [ -z "$CODE_PART" ]; then RC=1; return; fi
-    if [ $DEBUG -eq 1 ]; then echo -e "$CODE_PART"; fi
+    if [ $DEBUG -eq 1 ]; then echo "$CODE_PART"; fi
 
-    CODE_LINES=`echo -e "$CODE_PART" | wc -l`
+    CODE_LINES=`echo "$CODE_PART" | wc -l`
     if [ $CODE_LINES -eq $reslines ]; then
         isunique=1
     elif [ $CODE_LINES -ne $explines ]; then
@@ -51,20 +51,20 @@ get_malloc_code()
     fi
 
     if [ $isunique -ne 1 ]; then
-        CODE_PART=`echo -e "$CODE_PART" | tail -n $taillines`
-        CODE_PART=`echo -e "$CODE_PART" | head -n $reslines`
+        CODE_PART=`echo "$CODE_PART" | tail -n $taillines`
+        CODE_PART=`echo "$CODE_PART" | head -n $reslines`
 
-        CODE_LINES=`echo -e "$CODE_PART" | wc -l`
+        CODE_LINES=`echo "$CODE_PART" | wc -l`
         if [ $CODE_LINES -ne $reslines ]; then RC=1; return; fi
     fi
 
-    CODE_CALL=`echo -e "$CODE_PART" | cut -d '
+    CODE_CALL=`echo "$CODE_PART" | cut -d '
 ' -f $alines | grep call`
     if [ -z "$CODE_CALL" ]; then RC=1; return; fi
-    if [ $DEBUG -eq 1 ]; then echo -e "CODE_CALL:\n$CODE_CALL"; fi
+    if [ $DEBUG -eq 1 ]; then echo "CODE_CALL:"; echo "$CODE_CALL"; fi
 
-    CODE_ADDR=`echo -e "$CODE_PART" | tail -n 1 | cut -d ':' -f 1 | tr -d [:blank:]`
+    CODE_ADDR=`echo "$CODE_PART" | tail -n 1 | cut -d ':' -f 1 | tr -d [:blank:]`
     if [ -z "$CODE_ADDR" ]; then RC=1; return; fi
-    if [ $DEBUG -eq 1 ]; then echo -e "CODE_ADDR:\n$CODE_ADDR"; fi
+    if [ $DEBUG -eq 1 ]; then echo "CODE_ADDR:"; echo "$CODE_ADDR"; fi
     IFS=''
 }