ugtrain/ugtrain

commit ed9458d6dea114e16af6d035eab533d9f8234dbd

system: split sleep_sec_unless_input() into two functions
It's more efficient and cleaner using sleep_sec_unless_input() for
a single fd only, to introduce sleep_sec_unless_input2() for two
fds and not to check the fds anymore.

src/ugtrain.cpp
@@ -1011,7 +1011,7 @@ prepare_dynmem:
 	handle_statmem_pie(opt->code_offs, cfg);
 
 	while (true) {
-		sleep_sec_unless_input(1, ifd, STDIN_FILENO);
+		sleep_sec_unless_input2(1, ifd, STDIN_FILENO);
 		ch = do_getch();
 		if (ch >= 0 && ch < CFGP_MAP_SIZE)
 			handle_input_char(ch, cfgp_map, pid, cfg, cfg_act);

src/discovery.cpp
@@ -368,7 +368,7 @@ void run_stage5_loop (list<CfgEntry> *cfg, i32 ifd, i32 pmask, pid_t pid,
 {
 	enum pstate pstate;
 	while (true) {
-		sleep_sec_unless_input(1, ifd, -1);
+		sleep_sec_unless_input(1, ifd);
 		read_dynmem_buf(cfg, NULL, ifd, pmask, 0, code_offs,
 				process_disc5_output, NULL);
 		pstate = check_process(pid, NULL);
@@ -399,7 +399,7 @@ void run_stage1234_loop (void *argp)
 	}
 
 	while (true) {
-		sleep_sec_unless_input(1, ifd, -1);
+		sleep_sec_unless_input(1, ifd);
 		rbytes = read(ifd, buf, sizeof(buf));
 		if (rbytes == 0 || (rbytes < 0 && errno == EAGAIN))
 			continue;

src/discovery.h
@@ -94,7 +94,7 @@ static inline void handle_pie (struct app_options *opt, list<CfgEntry> *cfg,
 
 	if (!opt->pure_statmem) {
 		while (true) {
-			sleep_sec_unless_input(1, ifd, -1);
+			sleep_sec_unless_input(1, ifd);
 			rbytes = read(ifd, buf, sizeof(buf));
 			if (rbytes == 0 ||
 			    (rbytes < 0 && errno == EAGAIN))

src/lib/system.h
@@ -100,12 +100,31 @@ static inline void sleep_msec (u32 msec)
 }
 
 /*
+ * sleep given seconds count unless there is input on given fd
+ *
+ * Not using va_list to keep this inline. Assumption: fd is valid.
+ */
+static inline void sleep_sec_unless_input (u32 sec, i32 fd)
+{
+	fd_set fs;
+	struct timeval tv;
+	i32 nfds = fd + 1;
+
+	FD_ZERO(&fs);
+	tv.tv_sec = sec;
+	tv.tv_usec = 0;
+
+	FD_SET(fd, &fs);
+
+	select(nfds, &fs, NULL, NULL, &tv);
+}
+
+/*
  * sleep given seconds count unless there is input on given fds
  *
- * Not using va_list to keep this inline. Two fds is the maximum.
- * Use -1 for the second fd if it's not required.
+ * Not using va_list to keep this inline. Assumption: fds are valid.
  */
-static inline void sleep_sec_unless_input (u32 sec, i32 fd1, i32 fd2)
+static inline void sleep_sec_unless_input2 (u32 sec, i32 fd1, i32 fd2)
 {
 	fd_set fs;
 	struct timeval tv;
@@ -115,10 +134,8 @@ static inline void sleep_sec_unless_input (u32 sec, i32 fd1, i32 fd2)
 	tv.tv_sec = sec;
 	tv.tv_usec = 0;
 
-	if (fd1 >= 0)
-		FD_SET(fd1, &fs);
-	if (fd2 >= 0)
-		FD_SET(fd2, &fs);
+	FD_SET(fd1, &fs);
+	FD_SET(fd2, &fs);
 
 	select(nfds, &fs, NULL, NULL, &tv);
 }
@@ -165,7 +182,11 @@ static inline void sleep_msec (u32 msec)
 #ifndef STDIN_FILENO
 #define STDIN_FILENO -1
 #endif
-static inline void sleep_sec_unless_input (u32 sec, i32 fd1, i32 fd2)
+static inline void sleep_sec_unless_input (u32 sec, i32 fd)
+{
+	sleep_sec(sec);
+}
+static inline void sleep_sec_unless_input2 (u32 sec, i32 fd1, i32 fd2)
 {
 	sleep_sec(sec);
 }

commit 8003bd68d88348aacc0a9d744acf57b3161aed2d

ugtrain: fix a regression with pure static memory
Pure static memory cheating is rarely used. Most stuff is dynamic.
This is why it hasn't been noticed earlier that it doesn't work
any more. The problem is that sleep_sec_unless_input2() is called
where ifd is -1. So set it to STDIN_FILENO as well in this case.

Fixes: ed9458d6dea1 ("system: split sleep_sec_unless_input() into two functions")

src/ugtrain.cpp
@@ -949,6 +949,10 @@ prepare_dynmem:
 	handle_pie(opt, cfg, ifd, ofd, pid, &rlist);
 	handle_statmem_pie(opt->code_offs, cfg);
 
+	// use sleep_sec_unless_input2() also for pure static memory
+	if (ifd < 0)
+		ifd = STDIN_FILENO;
+
 	while (true) {
 		sleep_sec_unless_input2(1, ifd, STDIN_FILENO);
 		ch = do_getch();