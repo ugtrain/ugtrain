ugtrain/ugtrain

commit 214cb8a403087d9e1d5a2ffc34af523e635d9e0f

ugtrain: ignore SIGINT when running scanmem
The 'watch' command in scanmem is aborted by SIGINT. At the moment
ugtrain exits in case of triggering that signal and with it scanmem
as well as the game exit. Hence ignore SIGINT in ugtrain so that it
is only handled by scanmem if scanmem is running.

src/ugtrain.cpp
@@ -29,6 +29,7 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <libgen.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -558,6 +559,7 @@ static pid_t run_game (struct app_options *opt, char *preload_lib)
 		};
 
 		restore_getch();
+		sigignore(SIGINT);
 
 		cout << "$ " << pcmdv[0] << " " << pcmdv[1]
 		     << " `pidof " << opt->proc_name

commit a1ab1a46829cd52efb57e0d3ebd0cae32127c87c

ugtrain: restore SIGINT handling after scanmem exit
SIGINT is ignored when running scanmem but its handling is never
restored. This results in trouble killing ugtrain after running
scanmem. So restore SIGINT handling to its default after scanmem is
terminated.

Fixes: GitHub issue #5
Fixes: 214cb8a40308 ("ugtrain: ignore SIGINT when running scanmem")

src/discovery.cpp
@@ -27,6 +27,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <limits.h>
+#include <signal.h>
 
 // local includes
 #include <lib/getch.h>
@@ -444,6 +445,7 @@ void process_discovery (struct app_options *opt, list<CfgEntry> *cfg,
 		if (opt->scanmem_pid > 0) {
 			wait_orphan(pid, opt->proc_name);
 			wait_proc(opt->scanmem_pid);
+			signal(SIGINT, SIG_DFL);
 		} else {
 			wait_orphan(pid, opt->proc_name);
 		}
@@ -458,8 +460,10 @@ void process_discovery (struct app_options *opt, list<CfgEntry> *cfg,
 			do_disc_pic_work(pid, opt, ifd, ofd, rlist);
 		else
 			wait_orphan(pid, opt->proc_name);
-		if (opt->scanmem_pid > 0)
+		if (opt->scanmem_pid > 0) {
 			wait_proc(opt->scanmem_pid);
+			signal(SIGINT, SIG_DFL);
+		}
 		sleep_msec(250);  // chance to read the final flush
 		kill_proc(worker_pid);
 		if (worker_pid < 0)

src/ugtrain.cpp
@@ -572,6 +572,8 @@ static pid_t run_game (struct app_options *opt, char *preload_lib)
 				  false, preload_lib);
 		if (pid > 0)
 			opt->scanmem_pid = pid;
+		else
+			signal(SIGINT, SIG_DFL);
 	} else {
 		cout << "$ " << cmd_str << " &" << endl;
 
@@ -943,6 +945,7 @@ prepare_dynmem:
 	} else if (opt->scanmem_pid > 0) {
 		wait_orphan(pid, opt->proc_name);
 		wait_proc(opt->scanmem_pid);
+		signal(SIGINT, SIG_DFL);
 		return 0;
 	}
 