4892f095b8805df187bbf5ce62c83d6c2826ea7f
Sebastian Parschauer <s.parschauer@gmx.de>
2015-06-19 08:10:48 +0200

    fifoparser: simplify input handling and parsing
    Doing frequent calculations is cumbersome and error-prone. This
    especially affects the input and parsing positions (ipos and ppos).
    So maintain an input and a parsing start pointer (istart and pstart)
    instead. Precalculate the maximum number of bytes to be read and
    reduce code duplication by restructuring. Also remove debug messages.
    With simpler code it is easier to review and these aren't required
    any more.
    
    Functionality remains almost completely the same.

--- src/fifoparser.cpp
+++ src/fifoparser.cpp
@@ -30,11 +30,13 @@ ssize_t read_dynmem_buf (list<CfgEntry> *cfg, void *argp, i32 ifd, i32 pmask,
 {
 	ptr_t mem_addr = 0, code_addr = 0, stack_offs = 0;
 	static ptr_t heap_start = 0;
-	static ssize_t ipos = 0, ilen = 0;
+	static ssize_t ilen = 0;
 	ulong mem_size = 0;
-	ssize_t tmp_ilen, ppos = 0;
-	static char ibuf[PIPE_BUF] = { 0 };
-	char *msg_start = ibuf, *msg_end = ibuf, *sep_pos = ibuf;
+	ssize_t tmp_ilen;
+	static char ibuf[PIPE_BUF + 1] = { 0 };
+	size_t max_read = sizeof(ibuf) - 1 - ilen;   // always '\0' at end
+	char *istart = ibuf + ilen;
+	char *msg_start = ibuf, *msg_end = ibuf, *pstart = ibuf, *sep_pos = ibuf;
 	char *lib_name = NULL;
 	char scan_ch;
 	struct post_parse pp;
@@ -43,31 +45,31 @@ ssize_t read_dynmem_buf (list<CfgEntry> *cfg, void *argp, i32 ifd, i32 pmask,
 	if (reverse) {
 		if (fd_offs == REVERSE_FST) {
 			// read heap start from file first
-			tmp_ilen = read(ifd, ibuf, 50);
+			tmp_ilen = read(ifd, istart, max_read);
 			if (tmp_ilen <= 0 && argp)
 				return -1;
-			if (sscanf(msg_start, "%c", &scan_ch) != 1)
+			if (sscanf(pstart, "%c", &scan_ch) != 1)
 				goto parse_err;
 			if (scan_ch == 'h') {
-				ppos = msg_start - ibuf + 1;
-				if (sscanf(ibuf + ppos, SCN_PTR, &heap_start) != 1)
+				pstart++;
+				if (sscanf(pstart, SCN_PTR, &heap_start) != 1)
 					goto parse_err;
+				pstart--;
 			}
 			// clear buffer again
-			memset(ibuf, 0, tmp_ilen);
+			memset(istart, 0, tmp_ilen);
 			// set file pointer to file end
 			fd_offs = lseek(ifd, 0, SEEK_END);
 		}
-		if (fd_offs < 0)
+		if (fd_offs <= 0)
 			return -1;
-		// we use ipos as read length here
-		ipos = fd_offs;
-		if ((unsigned) ipos > sizeof(ibuf) - 1 - ilen) {
-			ipos = sizeof(ibuf) - 1 - ilen;
-			fd_offs = lseek(ifd, fd_offs - ipos, SEEK_SET);
+		// set file offset
+		if ((unsigned) fd_offs > max_read) {
+			fd_offs = lseek(ifd, fd_offs - max_read, SEEK_SET);
 			if (fd_offs < 0)
 				return -1;
 		} else {
+			max_read = (unsigned) fd_offs;
 			fd_offs = lseek(ifd, 0, SEEK_SET);
 			if (fd_offs < 0)
 				return -1;
@@ -75,20 +77,16 @@ ssize_t read_dynmem_buf (list<CfgEntry> *cfg, void *argp, i32 ifd, i32 pmask,
 		}
 		// move rest to the end
 		if (ilen)
-			memmove(ibuf + ipos, ibuf, ilen);
+			memmove(ibuf + max_read, ibuf, ilen);
 
 		// read from FIFO and concat. incomplete msgs
-		tmp_ilen = read(ifd, ibuf, ipos);  // always '\0' at end
+		tmp_ilen = read(ifd, ibuf, max_read);  // always '\0' at end
 	} else {
 		// read from FIFO and concat. incomplete msgs
-		tmp_ilen = read(ifd, ibuf + ipos, sizeof(ibuf) - 1 - ilen);  // always '\0' at end
+		tmp_ilen = read(ifd, istart, max_read);  // always '\0' at end
 	}
 	if (tmp_ilen > 0) {
-		//cout << "ibuf: " << string(ibuf) << endl;
 		ilen += tmp_ilen;
-		//cout << "ilen: " << ilen << " tmp_ilen " << tmp_ilen << endl;
-		//cout << "ipos: " << ipos << endl;
-		ipos += tmp_ilen;
 
 		// parse messages
 next:
@@ -104,65 +102,66 @@ next:
 				*msg_end = '\n';
 				return tmp_ilen;
 			} else {
-				msg_start += 1;
+				msg_start++;
 			}
 		} else {
 			msg_end = strchr(ibuf, '\n');
 			if (msg_end == NULL)
 				return tmp_ilen;
 		}
+		pstart = msg_start;
 
-		if (sscanf(msg_start, "%c", &scan_ch) != 1)
+		if (sscanf(pstart, "%c", &scan_ch) != 1)
 			goto parse_err;
 		switch (scan_ch) {
 		case 'm':
 			if (!pcb || !pcb->mf)
 				break;
-			ppos = msg_start - ibuf + 1;
-			if (sscanf(ibuf + ppos, SCN_PTR, &mem_addr) != 1)
+			pstart++;
+			if (sscanf(pstart, SCN_PTR, &mem_addr) != 1)
 				goto parse_err;
 
 			if (!(pmask & PARSE_S))
 				goto skip_s;
-			sep_pos = strchr(ibuf + ppos, ';');
+			sep_pos = strchr(pstart, ';');
 			if (!sep_pos)
 				goto parse_err;
-			ppos = sep_pos + 1 - ibuf;
-			if (sscanf(ibuf + ppos, "%c", &scan_ch) != 1 ||
+			pstart = sep_pos + 1;
+			if (sscanf(pstart, "%c", &scan_ch) != 1 ||
 			    scan_ch != 's')
 				goto parse_err;
-			ppos++;
-			if (sscanf(ibuf + ppos, "%lu", &mem_size) != 1)
+			pstart++;
+			if (sscanf(pstart, "%lu", &mem_size) != 1)
 				goto parse_err;
 skip_s:
 			if (!(pmask & PARSE_C))
 				goto skip_c;
-			sep_pos = strchr(ibuf + ppos, ';');
+			sep_pos = strchr(pstart, ';');
 			if (!sep_pos)
 				goto parse_err;
-			ppos = sep_pos + 1 - ibuf;
-			if (sscanf(ibuf + ppos, "%c", &scan_ch) != 1 ||
+			pstart = sep_pos + 1;
+			if (sscanf(pstart, "%c", &scan_ch) != 1 ||
 			    scan_ch != 'c')
 				goto parse_err;
-			ppos++;
-			if (sscanf(ibuf + ppos, SCN_PTR, &code_addr) != 1)
+			pstart++;
+			if (sscanf(pstart, SCN_PTR, &code_addr) != 1)
 				goto parse_err;
 skip_c:
 			if (!(pmask & PARSE_O))
 				goto skip_o;
-			sep_pos = strchr(ibuf + ppos, ';');
+			sep_pos = strchr(pstart, ';');
 			if (!sep_pos)
 				goto parse_err;
-			ppos = sep_pos + 1 - ibuf;
-			if (sscanf(ibuf + ppos, "%c", &scan_ch) != 1 ||
+			pstart = sep_pos + 1;
+			if (sscanf(pstart, "%c", &scan_ch) != 1 ||
 			    scan_ch != 'o')
 				goto parse_err;
-			ppos++;
-			if (sscanf(ibuf + ppos, SCN_PTR, &stack_offs) != 1)
+			pstart++;
+			if (sscanf(pstart, SCN_PTR, &stack_offs) != 1)
 				goto parse_err;
 skip_o:
 			pp.ibuf = ibuf;
-			pp.ppos = ppos;
+			pp.ppos = pstart - ibuf;
 			pp.argp = argp;
 
 			// call post parsing function
@@ -172,8 +171,8 @@ skip_o:
 		case 'f':
 			if (!pcb || !pcb->ff)
 				break;
-			ppos = msg_start - ibuf + 1;
-			if (sscanf(ibuf + ppos, SCN_PTR, &mem_addr) != 1)
+			pstart++;
+			if (sscanf(pstart, SCN_PTR, &mem_addr) != 1)
 				goto parse_err;
 
 			// call post parsing function
@@ -182,42 +181,37 @@ skip_o:
 		case 'l':
 			if (!pcb || !pcb->lf)
 				break;
-			ppos = msg_start - ibuf + 1;
-			if (ibuf[ppos] != ';')
+			pstart++;
+			if (*pstart != ';')
 				goto parse_err;
-			ppos++;
-			lib_name = new char[msg_end - (ibuf + ppos) + 1];
-			memcpy(lib_name, ibuf + ppos, msg_end - (ibuf + ppos));
-			lib_name[msg_end - (ibuf + ppos)] = '\0';
+			pstart++;
+			lib_name = new char[msg_end - pstart + 1];
+			memcpy(lib_name, pstart, msg_end - pstart);
+			lib_name[msg_end - pstart] = '\0';
 
 			// call post parsing function
 			pcb->lf(argp, lib_name);
 			delete lib_name;
 			break;
 		case 'h':
-			ppos = msg_start - ibuf + 1;
-			if (sscanf(ibuf + ppos, SCN_PTR, &heap_start) != 1)
+			pstart++;
+			if (sscanf(pstart, SCN_PTR, &heap_start) != 1)
 				goto parse_err;
 			break;
 		}
 
 		// prepare for next msg parsing
+		tmp_ilen = msg_end - msg_start + 1;
 		if (reverse) {
 			// zero parsed message
-			tmp_ilen = msg_end - msg_start + 1;
 			memset(msg_start, 0, tmp_ilen);
-			ilen -= tmp_ilen;
-			ipos = 0;
 		} else {
-			tmp_ilen = msg_end + 1 - ibuf;
 			// move rest to the front
 			memmove(ibuf, ibuf + tmp_ilen, ilen - tmp_ilen);
 			// zero what's behind the rest
 			memset(ibuf + ilen - tmp_ilen, 0, tmp_ilen);
-			ilen -= tmp_ilen;
-			ipos -= tmp_ilen;
-			//cout << "ilen: " << ilen << " ipos " << ipos << endl;
 		}
+		ilen -= tmp_ilen;
 
 		goto next;
 	} else if (argp) {
@@ -226,10 +220,9 @@ skip_o:
 	return tmp_ilen;
 
 parse_err:
-	cerr << "parse error at ppos: " << ppos << endl;
+	cerr << "parse error at ppos: " << pstart - ibuf << endl;
 	cerr << ibuf;
 	memset(ibuf, 0, sizeof(ibuf));
 	ilen = 0;
-	ipos = 0;
 	return 0;
 }
