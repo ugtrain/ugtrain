15185a6c352129192c759b21d2b76efd1a74e4f5
Sebastian Parschauer <s.parschauer@gmx.de>
2017-11-28 22:28:55 +0100

    adapt: warzone2100: Add 3.2.x 64 bit support
    Change from 'if' to 'case' to support the new version. Object sizes
    are different and the Structure object has one more expected line.

--- examples/adapt/warzone2100_adapt64.sh
+++ examples/adapt/warzone2100_adapt64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Tested with: Warzone 2100 2.3.8, 2.3.9, 3.1~rc2, 3.1.0, 3.1.1
+# Tested with: Warzone 2100 2.3.8, 2.3.9, 3.1~rc2, 3.1.0, 3.1.1, 3.2.3
 
 # We already know that warzone2100 is a 64-bit C++ application and that
 # the Droid and Structure classes are allocated with _Znwm() or malloc().
@@ -11,39 +11,76 @@ cd "$CWD"
 APP_PATH="$1"
 APP_VERS=`${APP_PATH} --version | grep -o "\([0-9]\+\\.\)\{2\}[0-9]\+"`
 RC=0
+
+# Droid size
 MSIZE11="0x410"
 MSIZE12="0x360"
+MSIZE13="0x368"
+
+# Structure size
 MSIZE21="0x160"
 MSIZE22="0x1a8"
+MSIZE23="0x170"
 
 . ./_common_adapt.sh
 
-if [ "$APP_VERS" = "2.3.8" -o "$APP_VERS" = "2.3.9" ]; then
+case "$APP_VERS" in
+2.3.*)
     MSIZE1="$MSIZE11"
     get_malloc_code "$APP_PATH" "\<malloc@plt\>" "$MSIZE1," 3 3 3
-else
+    ;;
+3.0*)
     MSIZE1="$MSIZE12"
     get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE1," 4 4 4
     if [ $RC -ne 0 ]; then
         get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE1," 3 3 3
     fi
-fi
+    ;;
+3.1*)
+    MSIZE1="$MSIZE12"
+    get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE1," 4 4 4
+    if [ $RC -ne 0 ]; then
+        get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE1," 3 3 3
+    fi
+    ;;
+*)
+    MSIZE1="$MSIZE13"
+    get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE1," 4 4 4
+    if [ $RC -ne 0 ]; then
+        get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE1," 3 3 3
+    fi
+    ;;
+esac
 if [ $RC -ne 0 ]; then exit 1; fi
 
 CODE_ADDR1="$CODE_ADDR"
 
 ############################################
 
-if [ "$APP_VERS" = "2.3.8" -o "$APP_VERS" = "2.3.9" ]; then
+case "$APP_VERS" in
+2.3.*)
     MSIZE2="$MSIZE21"
     get_malloc_code "$APP_PATH" "\<malloc@plt\>" "$MSIZE2," 3 7 7
-else
+    ;;
+3.0*)
     MSIZE2="$MSIZE22"
     get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE2," 4 8 4
     if [ $RC -ne 0 ]; then
         get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE2," 3 7 7
     fi
-fi
+    ;;
+3.1*)
+    MSIZE2="$MSIZE22"
+    get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE2," 4 8 4
+    if [ $RC -ne 0 ]; then
+        get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE2," 3 7 7
+    fi
+    ;;
+*)
+    MSIZE2="$MSIZE23"
+    get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE2," 4 9 4
+    ;;
+esac
 if [ $RC -ne 0 ]; then exit 1; fi
 
 CODE_ADDR2="$CODE_ADDR"
