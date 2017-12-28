ugtrain/ugtrain

commit 2ec26505b823fcb401169bddfc83ffbbe495ecbc

ugtrain: Add value on stack support
The config parser sets type.on_stack for config and check entries
already and activates opt->val_on_stack if values on the stack are
configured.

Skip static memory value output and processing if type.on_stack is
set. Send a config "1;0;0x0;0x0;stack" to libmemhack if there are
only values on the stack but no dynmem blocks as it always needs at
least one config entry. Skip early PIC/PIE handling in that case.
Always send the __libc_stack_end with prefix 'S', e.g.
"S0x7ffd8b323890\n" to ugtrain. Fix reading the "ready\n" string
so that it doesn't block parsing this. Subtract the offset of all
stack values and checks from the stack end and clear type.on_stack
to handle these values just like regular static memory values.

As the game has to fill the stack with permanent values first,
delay execution by a second if values on stack are found.

Reference: #44

src/ugtrain.cpp
@@ -211,6 +211,8 @@ static i32 process_checks (pid_t pid, DynMemEntry *dynmem,
 
 	list_for_each (chk_lp, it) {
 		chk_en = &(*it);
+		if (chk_en->type.on_stack)
+			continue;
 		if (chk_en->cfg_ref) {
 			ret = handle_cfg_ref(chk_en->cfg_ref, chk_buf);
 			if (ret)
@@ -466,6 +468,7 @@ static inline void read_dynmem_fifo (list<CfgEntry> *cfg,
 	pcb.lf = get_lib_load_addr;
 	pcb.mf = queue_dynmem_addr;
 	pcb.ff = clear_dynmem_addr;
+	pcb.sf = get_stack_end;
 
 	do {
 		rbytes = read_dynmem_buf(cfg, dp, ifd, pmask, false,
@@ -522,6 +525,8 @@ static void process_act_cfg (pid_t pid, list<CfgEntry*> *cfg_act)
 		} else {
 			mem_offs = 0;
 
+			if (cfg_en->type.on_stack)
+				continue;
 			mem_addr = cfg_en->addr;
 			if (read_memory(pid, mem_addr, buf, "MEMORY"))
 				continue;
@@ -747,6 +752,12 @@ static i32 prepare_dynmem (struct app_options *opt, list<CfgEntry> *cfg,
 	if (opt->use_gbt) {
 		pos += snprintf(obuf + pos, sizeof(obuf) - pos,
 			";%s", GBT_CMD);
+	}
+	// Libmemhack always needs a config. So provide a reserved one. */
+	if (opt->val_on_stack) {
+		num_cfg++;
+		pos += snprintf(obuf + pos, sizeof(obuf) - pos,
+			";0;0x0;0x0;stack");
 	}
 
 	// Fill the output buffer with the dynmem cfg

src/aslr.cpp
@@ -293,7 +293,7 @@ void handle_pie (struct app_options *opt, list<CfgEntry> *cfg, i32 ifd,
 	if (!opt->pure_statmem) {
 		while (true) {
 			sleep_sec_unless_input(1, ifd);
-			rbytes = read(ifd, buf, sizeof(buf));
+			rbytes = read(ifd, buf, sizeof(ASLR_NOTIFY_STR));
 			if (rbytes == 0 ||
 			    (rbytes < 0 && errno == EAGAIN))
 				continue;
@@ -362,4 +362,55 @@ void handle_pie (struct app_options *opt, list<CfgEntry> *cfg, i32 ifd,
 	wbytes = write(ofd, obuf, osize);
 	if (wbytes < osize)
 		perror("FIFO write");
+}
+
+// ################################
+// ###### Stack end handling ######
+// ################################
+
+/*
+ * Calculate the current virtual memory address for stack values:
+ * addr = stack_end - addr         This is x86 specific.
+ * Look for type.on_stack, then clear it to handle those values as
+ * regular static memory values. As long as type.on_stack is set,
+ * those aren't processed.
+ */
+
+// sf() callback for read_dynmem_buf()
+void get_stack_end (SF_PARAMS)
+{
+	list<CfgEntry>::iterator cfg_it;
+	CfgEntry *cfg_en;
+	list<CheckEntry> *chk_lp;
+	list<CheckEntry>::iterator chk_it;
+	CheckEntry *chk_en;
+	bool val_on_stack = false;
+
+	cout << "stack_end: 0x" << hex << stack_end << dec << endl;
+
+	list_for_each (cfg, cfg_it) {
+		cfg_en = &(*cfg_it);
+		if (cfg_en->dynmem || cfg_en->ptrmem)
+			continue;
+		if (cfg_en->type.on_stack) {
+			cfg_en->addr = stack_end - cfg_en->addr;
+			cfg_en->type.on_stack = false;
+			val_on_stack = true;
+		}
+
+		chk_lp = cfg_en->checks;
+		if (!chk_lp)
+			continue;
+		list_for_each (chk_lp, chk_it) {
+			chk_en = &(*chk_it);
+			if (!chk_en->type.on_stack)
+				continue;
+			chk_en->addr = stack_end - chk_en->addr;
+			chk_en->type.on_stack = false;
+			val_on_stack = true;
+		}
+	}
+	// HACK: Wait for the game to fill the stack with permanent data.
+	if (val_on_stack)
+		sleep_sec(1);
 }

src/aslr.h
@@ -37,4 +37,6 @@ void do_disc_pic_work (pid_t pid, struct app_options *opt,
 
 void get_lib_load_addr (LF_PARAMS);
 
+void get_stack_end (SF_PARAMS);
+
 #endif

src/common.h
@@ -32,7 +32,8 @@ enum grow_type {
 #define GBT_CMD     "gbt"  /* GNU backtrace() activation cmd */
 #define MAX_GNUBT   4      /* for GNU backtrace() */
 #define UGT_GAME_PROC_NAME     "UGT_GAME_PROC_NAME"
-#define DBG_FILE_NAME "/tmp/lib_dbg.txt"
+#define DBG_FILE_NAME          "/tmp/lib_dbg.txt"
+#define ASLR_NOTIFY_STR        "ready\n"
 
 /* Branch optimization */
 #define likely(x)     __builtin_expect(!!(x), 1)

src/fifoparser.cpp
@@ -28,7 +28,7 @@ ssize_t parse_dynmem_buf (list<CfgEntry> *cfg, void *argp, char *ibuf,
 			  bool reverse, ptr_t code_offs, struct parse_cb *pcb)
 {
 	static ptr_t heap_start = 0;
-	ptr_t mem_addr = 0, code_addr = 0, stack_offs = 0;
+	ptr_t mem_addr = 0, code_addr = 0, stack_offs = 0, stack_end = 0;
 	ulong mem_size = 0;
 	char *msg_start = ibuf, *msg_end = ibuf, *pstart = ibuf, *sep_pos = ibuf;
 	char *lib_name = NULL;
@@ -59,7 +59,7 @@ next:
 			return tmp_ilen;
 	}
 	pstart = msg_start;
-		if (sscanf(pstart, "%c", &scan_ch) != 1)
+	if (sscanf(pstart, "%c", &scan_ch) != 1)
 		goto parse_err;
 	switch (scan_ch) {
 	case 'm':
@@ -146,6 +146,18 @@ skip_o:
 		pstart++;
 		if (sscanf(pstart, SCN_PTR, &heap_start) != 1)
 			goto parse_err;
+		break;
+	case 'S':
+		if (!pcb || !pcb->sf)
+			break;
+		pstart++;
+		if (sscanf(pstart, SCN_PTR, &stack_end) != 1)
+			goto parse_err;
+		if (!stack_end)
+			break;
+
+		// call post parsing function
+		pcb->sf(cfg, stack_end);
 		break;
 	}
 

src/fifoparser.h
@@ -71,11 +71,15 @@ struct post_parse {
 #define LF_PARAMS \
 	void *argp, char *lib_name
 
+#define SF_PARAMS \
+	list<CfgEntry> *cfg, ptr_t stack_end
+
 /* parsing call back functions */
 struct parse_cb {
 	void (*mf)(MF_PARAMS);
 	void (*ff)(FF_PARAMS);
 	void (*lf)(LF_PARAMS);
+	void (*sf)(SF_PARAMS);
 };
 
 ssize_t read_dynmem_buf (list<CfgEntry> *cfg, void *argp, i32 ifd, i32 pmask,

src/linuxhooking/libmemdisc.c
@@ -480,13 +480,11 @@ void __attribute ((constructor)) memdisc_init (void)
 	if (stage >= 3 || ptr_cfg.code_addr != 0) {
 		/* notify ugtrain to do the PIE handling */
 		ssize_t wbytes;
-#define NOTIFY_STR "ready\n"
-		wbytes = write(ofd, NOTIFY_STR, sizeof(NOTIFY_STR));
-		if (wbytes != sizeof(NOTIFY_STR)) {
+		wbytes = write(ofd, ASLR_NOTIFY_STR, sizeof(ASLR_NOTIFY_STR));
+		if (wbytes != sizeof(ASLR_NOTIFY_STR)) {
 			pr_err("PIE handling: Can't write notification.\n");
 			goto out;
 		}
-#undef NOTIFY_STR
 		/* PIE handling if exe filter is requested */
 		if (bt_filter && bt_filter[0] == '\0') {
 			ptr_t exe_start = 0, exe_end = 0, exe_offs = 0;

src/linuxhooking/libmemhack.c
@@ -232,6 +232,7 @@ void __attribute ((constructor)) memhack_init (void)
 
 	if (read_input(ibuf, sizeof(ibuf) - 1) != 0)
 		goto read_err;
+	pr_dbg("raw config: %s", ibuf);
 
 	scanned = sscanf(start, "%u;", &num_cfg);
 	if (scanned != 1 || num_cfg <= 0)
@@ -325,15 +326,24 @@ void __attribute ((constructor)) memhack_init (void)
 	pr_dbg("num_cfg: %u\n", num_cfg);
 	output_cfg();
 
-	wbytes = write(ofd, "ready\n", sizeof("ready\n"));
-	if (wbytes < 0)
-		perror("write");
+	/* ugtrain waits for signal ready for ASLR handling */
+	wbytes = write(ofd, ASLR_NOTIFY_STR, sizeof(ASLR_NOTIFY_STR));
+	if (wbytes != sizeof(ASLR_NOTIFY_STR))
+		pr_err("PIE handling: Can't write notification.\n");
+
+	/* check for pure stack value config */
+	if (num_cfg == 1 && !config[0]->mem_size && !config[0]->grow)
+		goto skip_aslr;
+
+	/* ASLR: early PIC/PIE handling */
 	if (read_input(ibuf, sizeof(ibuf) - 1) != 0) {
 		pr_err("Couldn't read code offsets!\n");
 	} else {
 		char *start = ibuf, *pos;
 		for (i = 0; i < num_cfg; i++) {
 			ptr_t code_offs = 0;
+			if (!config[i]->mem_size && !config[i]->grow)
+				continue;
 			if (sscanf(start, SCN_PTR ";", &code_offs) < 1) {
 				pr_err("Code offset parsing error!\n");
 			} else {
@@ -364,8 +374,16 @@ void __attribute ((constructor)) memhack_init (void)
 		pr_out("Config after early PIC/PIE handling:\n");
 		output_cfg();
 	}
-	if (num_cfg > 0)
+skip_aslr:
+	if (num_cfg > 0) {
 		active = true;
+
+		/* report the stack end to ugtrain */
+		wbytes = sprintf(obuf, "S%p\n", __libc_stack_end);
+		wbytes = write(ofd, obuf, wbytes);
+		if (wbytes < 0)
+			perror("write");
+	}
 out:
 	return;
 

commit 5365caf036f78015ae72897de895096487156b97

ugtrain: PIE: Skip adding exe offset for values on stack
The function handle_statmem_pie() is called before gathering
__libc_stack_end and values on the stack are treated like static
memory in there. This leads to a wrong address for stack values
with a position-independent executable. So skip adding the exe
offset if the on_stack flag is set.

Found by code reading. This is untested as the game endless-sky
is not compiled as a PIE on openSUSE Leap 42.3 and on Ubuntu 16.04.

Fixes: 2ec26505b823 ("ugtrain: Add value on stack support")

src/ugtrain.cpp
@@ -586,12 +586,16 @@ static inline void handle_statmem_pie (ptr_t code_offs, list<CfgEntry> *cfg)
 	list_for_each (cfg, it) {
 		if (it->dynmem || it->ptrmem)
 			continue;
-		it->addr += code_offs;
+		if (!it->type.on_stack)
+			it->addr += code_offs;
 		if (!it->checks)
 			continue;
 		chk_lp = it->checks;
-		list_for_each (chk_lp, chk_it)
+		list_for_each (chk_lp, chk_it) {
+			if (chk_it->type.on_stack)
+				continue;
 			chk_it->addr += code_offs;
+		}
 	}
 }
 