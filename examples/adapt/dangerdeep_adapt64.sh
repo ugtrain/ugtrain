cb5ca153978da887843e09a20e6bf3c4fb712426
Sebastian Parschauer <s.parschauer@gmx.de>
2017-12-29 21:10:05 +0100

    examples: dangerdeep: Add adaption and heap ptr checks
    The Danger from the Deep code does not change frequently any more.
    It is safe to assume that the offsets within objects and data types
    will stay constant for quite some time.
    
    So add adaption and heap pointer checks to the stored torpedos
    vector start and end pointer.

---
+++ examples/adapt/dangerdeep_adapt64.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Tested with: dangerdeep 0.4.0
+
+# We already know that dangerdeep is a 64-bit C++ application. Therefore,
+# objects are allocated most likely by the "_Znwm" function. This function
+# calls malloc internally.
+# And from previous discovery runs we already know the malloc size (1784 or
+# 0x6f8) for the Submarine object.
+
+CWD=`dirname $0`
+cd "$CWD"
+APP_PATH="$1"
+APP_PATHS="\
+/usr/bin/dangerdeep
+/usr/games/dangerdeep
+"
+RC=0
+MSIZE="0x6f8"
+
+. ./_common_adapt.sh
+
+# check for changed paths
+get_app_path "$APP_PATH" "$APP_PATHS"
+
+get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE," 5 17 17
+if [ $RC -ne 0 ]; then exit 1; fi
+
+RESULT=`echo "1;${PATH_RESULT}Submarine;$MSIZE;0x$CODE_ADDR"`
+echo "$RESULT"
