ugtrain/ugtrain

commit 24fccb8faf878bee8e38e2c12ddad59b607e3437

ugtrain: Introduce /proc/$pid/mem I/O in 2K chunks
To reduce the number of syscalls for dynmem entries further, read
and write the caches in 2K chunks from/to /proc/$pid/mem the same
way as scanmem does. Fail incomplete reads/writes for now as those
are really unlikely.
Add the procmem file descriptor to the options, pass it over to
memattach_test(), and open the file. Pass the fd as the "pid"
parameter to the active config processing all the way through to
target memory reading and writing with pread() and pwrite(). Also
pass the cache size down.

Reference: #52

src/lib/memattach_l.h
@@ -35,7 +35,7 @@
 
 
 static inline
-i32 memattach_test (pid_t pid)
+i32 memattach_test (pid_t pid, i32 *fd)
 {
 	if (pid <= 1)
 		goto err;
@@ -46,7 +46,14 @@ i32 memattach_test (pid_t pid)
 		goto err;
 
 	waitpid(pid, NULL, 0);  /* wait for victim process stop */
+#ifdef HAVE_PROCMEM
+	char path[32];
 
+	snprintf(path, sizeof(path), "/proc/%d/mem", pid);
+	*fd = open(path, O_RDWR);
+	if (*fd < 0)
+		goto err;
+#endif
 	ptrace(PTRACE_DETACH, pid, 0, SIGCONT);
 	if (errno != 0)
 		goto err;
@@ -97,12 +104,19 @@ static inline
 i32 memread (pid_t pid, ptr_t addr, void *_buf, size_t buf_len)
 {
 	char *buf = (char *) _buf;
-	ptr_t read_val = 0;
-	size_t pos = 0;      /* position in sizeof(ptr_t) steps */
-	size_t len = buf_len / sizeof(ptr_t);
 
 	if (pid <= 1 || addr == 0 || buf == NULL || buf_len <= 0)
 		return -1;
+
+#ifdef HAVE_PROCMEM
+	i32 fd = (i32) pid;
+	/* TODO: Handle unlikely incomplete reads properly */
+	if (pread(fd, buf, buf_len, addr) < (ssize_t) buf_len)
+		goto err;
+#else
+	ptr_t read_val = 0;
+	size_t pos = 0;      /* position in sizeof(ptr_t) steps */
+	size_t len = buf_len / sizeof(ptr_t);
 
 	errno = 0;
 	for (pos = 0; pos < len; pos++) {
@@ -122,7 +136,7 @@ i32 memread (pid_t pid, ptr_t addr, void *_buf, size_t buf_len)
 			goto err;
 		memcpy(&buf[pos * sizeof(ptr_t)], &read_val, len);
 	}
-
+#endif
 	return 0;
 err:
 	return -1;
@@ -132,12 +146,19 @@ static inline
 i32 memwrite (pid_t pid, ptr_t addr, void *_buf, size_t buf_len)
 {
 	char *buf = (char *) _buf;
-	ptr_t rw_val = 0;
-	size_t pos = 0;      /* position in sizeof(ptr_t) steps */
-	size_t len = buf_len / sizeof(ptr_t);
 
 	if (pid <= 1 || addr == 0 || buf == NULL || buf_len <= 0)
 		return -1;
+
+#ifdef HAVE_PROCMEM
+	i32 fd = (i32) pid;
+	/* TODO: Handle unlikely incomplete writes properly */
+	if (pwrite(fd, buf, buf_len, addr) < (ssize_t) buf_len)
+		goto err;
+#else
+	ptr_t rw_val = 0;
+	size_t pos = 0;      /* position in sizeof(ptr_t) steps */
+	size_t len = buf_len / sizeof(ptr_t);
 
 	errno = 0;
 	for (pos = 0; pos < len; pos++) {
@@ -160,7 +181,7 @@ i32 memwrite (pid_t pid, ptr_t addr, void *_buf, size_t buf_len)
 		if (errno != 0)
 			goto err;
 	}
-
+#endif
 	return 0;
 err:
 	return -1;

src/cfgentry.h
@@ -20,8 +20,12 @@
 // local includes
 #include <common.h>
 
+#ifdef HAVE_PROCMEM
+#define MEM_CHUNK 2048                // r/w chunk size for caching target memory
+#else
+#define MEM_CHUNK sizeof(value_t)
+#endif
 
-#define MEM_CHUNK sizeof(value_t)     // r/w chunk size for caching target memory
 #define PTR_MAX ULONG_MAX             // value to show that a cache is invalid
 
 class CacheEntry {

src/options.c
@@ -127,6 +127,7 @@ static void init_options (struct app_options *opt)
 	memset(opt, 0, sizeof(struct app_options));
 
 	/* no direct CLI input */
+	opt->procmem_fd = -1;
 	opt->scanmem_pid = -1;
 	opt->dynmem_file = DYNMEM_FILE;
 }

src/options.h
@@ -36,6 +36,7 @@ struct app_options {
 	bool	run_scanmem;
 	bool	use_glc;
 	/* no direct CLI input */
+	i32	procmem_fd;
 	bool	pure_statmem;
 	bool	have_objdump;
 	bool    val_on_stack;

src/ugtrain.cpp
@@ -211,8 +211,8 @@ static i32 process_checks (pid_t pid, DynMemEntry *dynmem,
 				// fill cache and fill chk_buf from cache
 				mem_addr = mem_offs + chk_en->cache->offs;
 				if (chk_en->cache->start == PTR_MAX) {
-					ret = read_memory(pid, mem_addr,
-						(value_t *) chk_en->cache->data, "MEMORY");
+					ret = _read_memory(pid, mem_addr,
+						chk_en->cache->data, MEM_CHUNK, "MEMORY");
 					if (ret)
 						goto out;
 					chk_en->cache->start = mem_addr;
@@ -546,8 +546,8 @@ process_dynmem_cfg_act (pid_t pid, list<CfgEntry*> *cfg_act,
 			// fill cache and fill buf from cache
 			mem_addr = mem_offs + cfg_en->cache->offs;
 			if (cfg_en->cache->start == PTR_MAX) {
-				if (read_memory(pid, mem_addr,
-				    (value_t *) cfg_en->cache->data, "MEMORY"))
+				if (_read_memory(pid, mem_addr,
+				    cfg_en->cache->data, MEM_CHUNK, "MEMORY"))
 					continue;
 				cfg_en->cache->start = mem_addr;
 			}
@@ -567,8 +567,8 @@ process_dynmem_cfg_act (pid_t pid, list<CfgEntry*> *cfg_act,
 			if (!cait->is_dirty)
 				continue;
 			cait->is_dirty = false;
-			if (write_memory(pid, cait->start,
-			    (value_t *) cait->data, "MEMORY"))
+			if (_write_memory(pid, cait->start,
+			    cait->data, MEM_CHUNK, "MEMORY"))
 				continue;
 		}
 	}
@@ -1154,7 +1154,7 @@ i32 main (i32 argc, char **argv, char **env)
 	if (set_getch_nb(1) != 0)
 		return -1;
 
-	test_memattach(pid);
+	test_memattach(pid, &opt->procmem_fd);
 
 	handle_aslr(opt, cfg, ifd, ofd, pid, rlist);
 	handle_statmem_pie(opt->code_offs, cfg);
@@ -1209,7 +1209,7 @@ i32 main (i32 argc, char **argv, char **env)
 			read_dynmem_fifo(cfg, dmparams, ifd, pmask);
 
 		// TIME CRITICAL! Process all activated config entries
-		process_act_cfg(pid, cfg_act);
+		process_act_cfg((opt->procmem_fd >= 0) ? opt->procmem_fd : pid, cfg_act);
 
 		detachmem(pid);
 

src/util.h
@@ -84,9 +84,9 @@ tool_is_available (char *name)
 }
 
 static inline void
-test_memattach (pid_t pid)
+test_memattach (pid_t pid, i32 *fd)
 {
-	if (likely(memattach_test(pid) == 0))
+	if (likely(memattach_test(pid, fd) == 0))
 		return;
 	cerr << "MEMORY ATTACHING TEST ERROR PID[" << pid << "]!" << endl;
 	exit(-1);
@@ -113,11 +113,11 @@ detachmem (pid_t pid)
 }
 
 static inline i32
-read_memory (pid_t pid, ptr_t mem_addr, value_t *buf, const char *pfx)
+_read_memory (pid_t pid, ptr_t mem_addr, void *buf, size_t buf_len, const char *pfx)
 {
 	i32 ret;
 
-	ret = memread(pid, mem_addr, buf, sizeof(value_t));
+	ret = memread(pid, mem_addr, buf, buf_len);
 	if (ret)
 		cerr << pfx << " READ ERROR PID[" << pid << "] ("
 		     << hex << mem_addr << dec << ")!" << endl;
@@ -125,15 +125,27 @@ read_memory (pid_t pid, ptr_t mem_addr, value_t *buf, const char *pfx)
 }
 
 static inline i32
-write_memory (pid_t pid, ptr_t mem_addr, value_t *buf, const char *pfx)
+read_memory (pid_t pid, ptr_t mem_addr, value_t *buf, const char *pfx)
+{
+	return _read_memory(pid, mem_addr, buf, sizeof(value_t), pfx);
+}
+
+static inline i32
+_write_memory (pid_t pid, ptr_t mem_addr, void *buf, size_t buf_len, const char *pfx)
 {
 	i32 ret;
 
-	ret = memwrite(pid, mem_addr, buf, sizeof(value_t));
+	ret = memwrite(pid, mem_addr, buf, buf_len);
 	if (ret)
 		cerr << pfx << " WRITE ERROR PID[" << pid << "] ("
 		     << hex << mem_addr << dec << ")!" << endl;
 	return ret;
+}
+
+static inline i32
+write_memory (pid_t pid, ptr_t mem_addr, value_t *buf, const char *pfx)
+{
+	return _write_memory(pid, mem_addr, buf, sizeof(value_t), pfx);
 }
 
 #endif

commit 605ed67223776f706f8ca0b581237c974a8a1225

memattach: Add a missing include
O_RDWR which is required for opening the file /proc/$pid/mem requires
the include fcntl.h. So add it.

Fixes: 24fccb8faf87 ("ugtrain: Introduce /proc/$pid/mem I/O in 2K chunks")

src/lib/memattach_l.h
@@ -26,6 +26,7 @@
 #define MEMATTACH_L_H
 
 #include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>