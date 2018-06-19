ugtrain/ugtrain

commit 2b6f291d1aee1520866769578d95d35945886b05

ugtrain: Enhance the cleanup with atexit()
For restoring the terminal, there is atexit() handling any way. So
instead of calling the cleanup function manually, it makes sense
to register it with atexit().
The problem is that the global variables of ugtrain must be available
to the atexit() handler. So introduce a new file "util.cpp" and
define those as static variables. Also introduce a getter function
to get those for the ugtrain main() function and to initialize them
in opt. Furthermore, fix the biggest one-time leaks reported by
Valgrind during discovery and split a clear_config() function off.
Fix a name conflict by renaming struct map to struct region_map and
of cause make the cleanup safer, so that it can be called early.

src/discovery.cpp
@@ -386,6 +386,7 @@ void process_discovery (Options *opt, list<CfgEntry> *cfg,
 		get_stack_start(opt, pid, rlist);
 		if (opt->disc_str[0] >= '3' || opt->disc_offs > 0)
 			handle_aslr(opt, cfg, ifd, ofd, pid, rlist);
+		rlist_clear(rlist);
 		worker_pid = fork_proc(run_stage1234_loop, &params);
 		if (opt->disc_str[0] >= '3' && opt->disc_lib &&
 		    opt->disc_lib[0] != '\0')
@@ -551,8 +552,7 @@ err:
 	return -1;
 }
 
-bool init_discovery (Options *opt, list<CfgEntry> *cfg,
-		     list<CfgEntry*> *cfg_act)
+bool init_discovery (Options *opt)
 {
 	bool allow_empty_cfg = false;
 
@@ -569,8 +569,7 @@ bool init_discovery (Options *opt, list<CfgEntry> *cfg,
 				    "lookup aren't available." << endl;
 		}
 		ugout << "Clearing config for discovery!" << endl;
-		cfg->clear();
-		cfg_act->clear();
+		clear_config();
 		allow_empty_cfg = true;
 	} else {
 		opt->run_scanmem = false;

src/Makefile.am
@@ -16,6 +16,7 @@ ugtrain_SOURCES = \
 	memmgmt.cpp \
 	options.cpp \
 	ugtrain.cpp \
+	util.cpp \
 	valoutput.cpp
 if LINUX
 ugtrain_SOURCES += \

src/aslr.cpp
@@ -56,7 +56,7 @@ static inline void send_lib_bounds (i32 ofd, ptr_t lib_start, ptr_t lib_end)
  *
  * Assumptions: disc_lib != NULL, disc_lib[0] != '\0'
  */
-static inline i32 send_bt_addrs (struct map *map, void *data)
+static inline i32 send_bt_addrs (struct region_map *map, void *data)
 {
 	struct lf_disc_params *lfp = (struct lf_disc_params *) data;
 	char *disc_lib = lfp->disc_lib;

src/discovery.h
@@ -30,8 +30,7 @@
 
 bool init_scanmem	(Options *opt, list<CfgEntry> *cfg,
 			 list<CfgEntry*> *cfg_act);
-bool init_discovery     (Options *opt, list<CfgEntry> *cfg,
-		         list<CfgEntry*> *cfg_act);
+bool init_discovery     (Options *opt);
 i32  prepare_discovery  (Options *opt, list<CfgEntry> *cfg);
 void process_discovery  (Options *opt, list<CfgEntry> *cfg,
 			 i32 ifd, i32 dfd, i32 ofd, i32 pid,

src/lib/maps.h
@@ -31,7 +31,7 @@
 #define MAPS_MAX_PATH 256
 
 /* a map obtained from /proc/$pid/maps */
-struct map {
+struct region_map {
 	ulong start;
 	ulong end;
 	char read, write, exec, cow;
@@ -42,9 +42,9 @@ struct map {
 #ifdef __cplusplus
 extern "C" {
 #endif
-	i32 read_maps   (pid_t pid, i32 (*callback)(struct map *map, void *data),
-			 void *data);
-	i32 process_map (struct map *map, void *data);
+	i32 read_maps   (pid_t pid, i32 (*callback)(struct region_map *map,
+			 void *data), void *data);
+	i32 process_map (struct region_map *map, void *data);
 	i32 write_maps_to_file (const char *path, pid_t pid);
 #ifdef __cplusplus
 };

src/lib/maps_l.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2006,2007,2009 Tavis Ormandy <taviso@sdf.lonestar.org>
  * Copyright (C) 2009           Eli Dupree <elidupree@charter.net>
  * Copyright (C) 2009,2010      WANG Lu <coolwanglu@gmail.com>
- * Copyright (C) 2014..2015     Sebastian Parschauer <s.parschauer@gmx.de>
+ * Copyright (C) 2014..2018     Sebastian Parschauer <s.parschauer@gmx.de>
  *
  * Code in this file has been taken from scanmem.
  *
@@ -79,7 +79,7 @@ const char *region_type_names[] = REGION_TYPE_NAMES;
  *
  * append all executable and writable regions to a regions list
  */
-i32 process_map (struct map *map, void *data)
+i32 process_map (struct region_map *map, void *data)
 {
 	struct pmap_params *params = (struct pmap_params *) data;
 	char *exe_path = params->exe_path;
@@ -232,7 +232,7 @@ error:
  * Assumption: pid > 0
  * Inspired by dl_iterate_phdr().
  */
-i32 read_maps (pid_t pid, i32 (*callback)(struct map *map, void *data),
+i32 read_maps (pid_t pid, i32 (*callback)(struct region_map *map, void *data),
 	       void *data)
 {
 	FILE *maps;
@@ -247,7 +247,7 @@ i32 read_maps (pid_t pid, i32 (*callback)(struct map *map, void *data),
 
 	/* read every line of the maps file, parse them and call the callback */
 	while (getline(&line, &len, maps) != -1) {
-		struct map map;
+		struct region_map map;
 
 		/* slight overallocation */
 		map.file_path = (char *) alloca(len);

src/lib/maps_w.c
@@ -27,7 +27,7 @@ const char *region_type_names[] = REGION_TYPE_NAMES;
 /*
  * callback for read_maps()
  */
-i32 process_map (struct map *map, void *data)
+i32 process_map (struct region_map *map, void *data)
 {
 	return -1;
 }
@@ -38,7 +38,7 @@ i32 process_map (struct map *map, void *data)
  * -1: fopen(), alloca() or sscanf() error,
  * return code of callback() != 0.
  */
-i32 read_maps (pid_t pid, i32 (*callback)(struct map *map, void *data),
+i32 read_maps (pid_t pid, i32 (*callback)(struct region_map *map, void *data),
 	       void *data)
 {
 	return -1;

src/options.cpp
@@ -122,6 +122,9 @@ void cleanup_options (Options *opt)
 	list<CacheEntry>::iterator cait;
 	list<LibEntry>::iterator lit;
 
+	// Free cache lists first
+	if (!opt->lib_list)
+		goto skip_lib_list;
 	list_for_each (opt->lib_list, lit) {
 		list_for_each (lit->cache_list, cait)
 			delete[] cait->data;
@@ -130,15 +133,23 @@ void cleanup_options (Options *opt)
 	}
 	opt->lib_list->clear();
 	delete opt->lib_list;
+skip_lib_list:
+	if (!opt->stack || !opt->stack->cache_list)
+		goto skip_stack_cache;
 	list_for_each (opt->stack->cache_list, cait)
 		delete[] cait->data;
 	opt->stack->cache_list->clear();
 	delete opt->stack->cache_list;
 	delete opt->stack;
+skip_stack_cache:
+	if (!opt->cache_list)
+		goto skip_statmem_cache;
 	list_for_each (opt->cache_list, cait)
 		delete[] cait->data;
 	opt->cache_list->clear();
 	delete opt->cache_list;
+
+skip_statmem_cache:
 	if (opt->cfg_path)
 		delete[] opt->cfg_path;
 	if (opt->dynmem_file)

commit 596cefab3a78aa39fb7fea32e31f4b606964e016

discovery: Drop freeing rlist to fix the backtrace
The stage 3/4 backtrace is broken as the regions list is empty at
the post-processing step. So remove clearing the regions list in the
middle of the discovery.
The right way would be to add an async-signal-safe signal handler
which sets a flag to inform the forked off process running
run_stage1234_loop() that it should exit regularly.

Fixes: 2b6f291d1aee ("ugtrain: Enhance the cleanup with atexit()")

src/discovery.cpp
@@ -386,7 +386,6 @@ void process_discovery (Options *opt, i32 ifd, i32 dfd, i32 ofd, i32 pid)
 		get_stack_start(opt, pid, rlist);
 		if (opt->disc_str[0] >= '3' || opt->disc_offs > 0)
 			handle_aslr(opt, cfg, ifd, ofd, pid, rlist);
-		rlist_clear(rlist);
 		worker_pid = fork_proc(run_stage1234_loop, &params);
 		if (opt->disc_str[0] >= '3' && opt->disc_lib &&
 		    opt->disc_lib[0] != '\0')