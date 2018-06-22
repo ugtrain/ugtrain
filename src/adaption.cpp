ugtrain/ugtrain

commit b90ce4376323bee0fd7142bdc49627886d68c6a0

ugtrain: Make Valgrind memory leak detection happy
A memory leak in the ugtrain main loop would be really annoying.
In order to use Valgrind for debugging, fix all the false-positives
and one-time leaks. Clean up memory when the game and therefore
ugtrain exits. Do not mix stack and heap allocations. Allocate
separate C strings for identical strings which can differ by config
changes. Furthermore, free memory before allocating new memory for
the same variable. Always use matching allocation and deletion
functions.
Also detect if the game has terminated itself if we cannot detach.

Tested with: valgrind --leak-check=full --show-reachable=yes <cmd>

src/adaption.cpp
@@ -156,13 +156,21 @@ static i32 parse_adapt_result (Options *opt, list<CfgEntry> *cfg,
 				if (lnr == 0) {
 					tmp_line = string(obj_name);
 					if (strcmp(opt->game_call, opt->proc_name) == 0) {
+						if (opt->proc_name)
+							delete[] opt->proc_name;
 						opt->proc_name = to_c_str(&tmp_line);
-						opt->game_call = opt->proc_name;
+						if (opt->game_call)
+							delete[] opt->game_call;
+						opt->game_call = to_c_str(&tmp_line);
 					} else {
+						if (opt->proc_name)
+							delete[] opt->proc_name;
 						opt->proc_name = to_c_str(&tmp_line);
 					}
 				} else if ((u32) lnr == opt->binpath_line) {
 					tmp_line = string(obj_name);
+					if (opt->game_binpath)
+						delete[] opt->game_binpath;
 					opt->game_binpath = to_c_str(&tmp_line);
 					if (strcmp(basename(opt->game_binpath),
 					    opt->proc_name) != 0)

src/cfgparser.cpp
@@ -355,10 +355,16 @@ static bool parse_obj_num_chk (list<CfgEntry> *cfg, CheckEntry *chk_en,
 
 static inline ptr_t get_cfg_en_addr (CheckEntry *chk_en, CfgEntry *cfg_en)
 {
-	if (cfg_en->type.on_stack)
+	string tmp_str;
+
+	if (cfg_en->type.on_stack) {
 		chk_en->type.on_stack = true;
-	else if (cfg_en->type.lib_name)
-		chk_en->type.lib_name = cfg_en->type.lib_name;
+	} else if (cfg_en->type.lib_name) {
+		if (chk_en->type.lib_name)
+			delete[] chk_en->type.lib_name;
+		tmp_str = cfg_en->type.lib_name;
+		chk_en->type.lib_name = to_c_str(&tmp_str);
+	}
 	return cfg_en->addr;
 }
 
@@ -898,7 +904,8 @@ void read_config (Options *opt,
 
 	// parse config
 	opt->proc_name = parse_proc_name(&lines->at(0), &start);
-	opt->game_call = opt->proc_name;
+	tmp_str = opt->proc_name;
+	opt->game_call = to_c_str(&tmp_str);
 
 	for (lnr = 1; lnr < lines->size(); lnr++) {
 		line = lines->at(lnr);
@@ -1011,6 +1018,8 @@ void read_config (Options *opt,
 
 			tmp_str = parse_value_name(&line, lnr,
 				&start, true, NULL);
+			if (opt->dynmem_file)
+				delete[] opt->dynmem_file;
 			opt->dynmem_file = to_c_str(&tmp_str);
 			break;
 
@@ -1082,7 +1091,7 @@ void read_config (Options *opt,
 				cfg_parse_err(&line, lnr, start);
 
 			// Copy into C string
-			opt->game_path = to_c_str(&tmp_str);
+			opt->game_path = to_c_str_c(&tmp_str);
 			break;
 
 		case NAME_GAME_CALL:
@@ -1093,6 +1102,8 @@ void read_config (Options *opt,
 				  lnr, &start, false, NULL);
 
 			// Copy into C string
+			if (opt->game_call)
+				delete[] opt->game_call;
 			opt->game_call = to_c_str(&tmp_str);
 			break;
 

src/options.cpp
@@ -117,14 +117,59 @@ static struct option long_options[] = {
 	{0, 0, 0, 0}
 };
 
+void cleanup_options (Options *opt)
+{
+	list<CacheEntry>::iterator cait;
+	list<LibEntry>::iterator lit;
+
+	list_for_each (opt->lib_list, lit) {
+		list_for_each (lit->cache_list, cait)
+			delete[] cait->data;
+		lit->cache_list->clear();
+		delete lit->cache_list;
+	}
+	opt->lib_list->clear();
+	delete opt->lib_list;
+	list_for_each (opt->stack->cache_list, cait)
+		delete[] cait->data;
+	opt->stack->cache_list->clear();
+	delete opt->stack->cache_list;
+	delete opt->stack;
+	list_for_each (opt->cache_list, cait)
+		delete[] cait->data;
+	opt->cache_list->clear();
+	delete opt->cache_list;
+	if (opt->cfg_path)
+		delete[] opt->cfg_path;
+	if (opt->dynmem_file)
+		delete[] opt->dynmem_file;
+	if (opt->game_path) {
+		free(opt->game_path);
+		opt->game_path = NULL;
+	}
+	if (opt->game_binpath)
+		delete[] opt->game_binpath;
+	if (opt->game_call)
+		delete[] opt->game_call;
+	if (opt->game_params)
+		delete[] opt->game_params;
+	if (opt->proc_name)
+		delete[] opt->proc_name;
+	if (opt->adp_script)
+		delete[] opt->adp_script;
+}
+
 static void init_options (Options *opt)
 {
+	string tmp_str;
+
 	memset(opt, 0, sizeof(Options));
 
 	/* no direct CLI input */
 	opt->procmem_fd = -1;
 	opt->scanmem_pid = -1;
-	opt->dynmem_file = (char *) DYNMEM_FILE;
+	tmp_str = DYNMEM_FILE;
+	opt->dynmem_file = to_c_str(&tmp_str);
 	opt->cache_list = new list<CacheEntry>;
 	opt->stack = new StackOpt;
 	opt->stack->cache_list = new list<CacheEntry>;

src/options.h
@@ -69,15 +69,19 @@ public:
 	ptr_t	heap_end;
 	size_t	disc_offs;
 	pid_t	scanmem_pid;
-	list<CacheEntry> *cache_list;  // caching static memory
-	list<LibEntry> *lib_list;      // handling statmem in libraries
-	StackOpt *stack;
+	list<CfgEntry>		*cfg;
+	list<CfgEntry*>		*cfg_act;
+	list<CfgEntry*>		**cfgp_map;
+	list<CacheEntry>	*cache_list;	// caching static memory
+	list<LibEntry>		*lib_list;	// handling statmem in libraries
+	StackOpt		*stack;
 	// options for testing
 	TESTING_OPT_VARS
 };
 
 void do_assumptions (Options *opt);
 void parse_options (i32 argc, char **argv, Options *opt);
+void cleanup_options (Options *opt);
 
 /* inline functions */
 static inline void use_libmemdisc (Options *opt)

src/cfgentry.h
@@ -32,7 +32,7 @@ class CacheEntry {
 public:
 	ptr_t offs;                   // offset within dynmem object
 	ptr_t start;                  // target memory address or cache valid?
-	void *data;                   // target memory r/w buffer
+	char *data;                   // target memory r/w buffer
 	bool is_dirty;                // writing back required?
 };
 

src/common.cpp
@@ -11,6 +11,7 @@
  * GNU General Public License for more details.
  */
 
+#include <cstdlib>
 #include <cstring>
 
 // local includes
@@ -20,6 +21,15 @@
 char *to_c_str (string *str)
 {
 	char *cstr = new char[str->size() + 1];
+
+	cstr[str->size()] = '\0';
+	memcpy(cstr, str->c_str(), str->size());
+	return cstr;
+}
+
+char *to_c_str_c (string *str)
+{
+	char *cstr = (char *) malloc(str->size() + 1);
 
 	cstr[str->size()] = '\0';
 	memcpy(cstr, str->c_str(), str->size());

src/common.h
@@ -62,6 +62,7 @@ enum grow_type {
 /* Common functions */
 #ifdef __cplusplus
 	char *to_c_str(string *str);
+	char *to_c_str_c(string *str);
 #endif
 
 #endif

src/memmgmt.cpp
@@ -39,6 +39,8 @@ static void alloc_ptrmem (CfgEntry *cfg_en)
 		if (cfg_en->type.is_cstrp)
 			cstr = (char *) calloc(1, MAX_CSTR + 1);
 		cfg_en->v_cstr.push_back(cstr);
+		if (cstr)
+			free(cstr);
 	}
 }
 
@@ -66,6 +68,8 @@ void alloc_dynmem (list<CfgEntry> *cfg)
 				if (cfg_en->type.is_cstrp)
 					cstr = (char *) calloc(1, MAX_CSTR + 1);
 				cfg_en->v_cstr.push_back(cstr);
+				if (cstr)
+					free(cstr);
 				if (cfg_en->ptrtgt)
 					alloc_ptrmem(cfg_en);
 			}

commit 4ef05ad4311836fc457e88fdea082caa8d8fa4ee

ugtrain: Use C++ string for config path
It is possible that the config path string is still located on the
stack and the cleanup tries to free it. This causes a segfault if
the path does not exist. So just convert opt->cfg_path to a C++
string, so that it is always located on the heap.

Fixes: b90ce4376323 ("ugtrain: Make Valgrind memory leak detection happy")

src/adaption.cpp
@@ -31,16 +31,16 @@
 #include <commont.cpp>
 
 
-static i32 write_config_vect (char *path, vector<string> *cfg_lines)
+static i32 write_config_vect (string *path, vector<string> *cfg_lines)
 {
 	ofstream cfg_file;
 	vector<string>::iterator it;
 
 	cfg_lines->pop_back();
 
-	cfg_file.open(path, fstream::trunc);
+	cfg_file.open(path->c_str(), fstream::trunc);
 	if (!cfg_file.is_open()) {
-		ugerr << "File \"" << path << "\" doesn't exist!" << endl;
+		ugerr << "File \"" << *path << "\" doesn't exist!" << endl;
 		return -1;
 	}
 	vect_for_each (cfg_lines, it)

src/cfgparser.cpp
@@ -899,9 +899,7 @@ void read_config (Options *opt, vector<string> *cfg_lines)
 	opt->stack->cache_list->push_back(cache_en);
 
 	// read config into string vector
-	tmp_str = string(opt->cfg_path);
-	read_config_vect(&tmp_str, opt->home, cfg_lines);
-	opt->cfg_path = to_c_str(&tmp_str);
+	read_config_vect(opt->cfg_path, opt->home, cfg_lines);
 
 	// parse config
 	opt->proc_name = parse_proc_name(&cfg_lines->at(0), &start);
@@ -1052,7 +1050,7 @@ void read_config (Options *opt, vector<string> *cfg_lines)
 			if (in_dynmem || in_ptrmem)
 				cfg_parse_err(&line, lnr, start);
 
-			tmp_str = string(opt->cfg_path);
+			tmp_str = *opt->cfg_path;
 			pos = tmp_str.rfind("/");
 			if (pos != string::npos)
 				tmp_str.erase(pos + 1, string::npos);

src/options.cpp
@@ -142,7 +142,7 @@ skip_stack_cache:
 
 skip_statmem_cache:
 	if (opt->cfg_path)
-		delete[] opt->cfg_path;
+		delete opt->cfg_path;
 	if (opt->dynmem_file)
 		delete[] opt->dynmem_file;
 	if (opt->game_path) {
@@ -175,6 +175,7 @@ static void init_options (Options *opt)
 	init_opt_globals(opt);
 	opt->procmem_fd = -1;
 	opt->scanmem_pid = -1;
+	opt->cfg_path = new string;
 	opt->disc_str = new string;
 	opt->disc_lib = new string;
 	tmp_str = DYNMEM_FILE;
@@ -250,12 +251,12 @@ void parse_options (i32 argc, char **argv, Options *opt)
 					break;
 				}
 			} else {
-				opt->cfg_path = argv[optind - 1];
+				*opt->cfg_path = argv[optind - 1];
 			}
 			break;
 		}
 	}
-	if (!opt->cfg_path)
+	if (opt->cfg_path->empty())
 	       usage();
 
 	do_assumptions(opt);

src/options.h
@@ -39,7 +39,7 @@ public:
 
 class Options {
 public:
-	char	*cfg_path;
+	string	*cfg_path;
 	char	*pre_cmd;
 	char	*preload_lib;
 	string	*disc_str;