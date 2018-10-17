ugtrain/ugtrain

commit 34c2cd745593d97f55f87ba52b46fdbd7a52cb91

system: Optimize check_process()
Executing shell commands for this function is really lazy and ugly.
So rather use fopen() to check if /proc/$pid/status exists. If it
does not, assume that the process is dead. Then read it with
getline(), but only read the first two lines. Use offsets to extract
the required information and assume that the output format does not
change. This is good for performance but less safe. Also handle
status 'X' like 'Z' for zombie processes and check if the status is
a capital letter.

Verified with Valgrind leak detection.

Fixes #62

src/lib/system_l.c
@@ -71,65 +71,52 @@ err:
  */
 enum pstate check_process (pid_t pid, const char *proc_name)
 {
-	char pbuf[PIPE_BUF] = { 0 };
-	DIR *dir;
-	const char *cmd;
-	char *pname;
-	char cmd_str[1024] = "/proc/";
-	i32 pr_len, cmd_len = sizeof("/proc/") - 1;
+	FILE *fp = NULL;
+	char *line = NULL;
+	size_t alloc_len = 0;
+	u32 lnr = 0;
+	char status = '\0';
+	char path_str[1024] = "/proc/";
+	i32 pr_len, path_len = sizeof("/proc/") - 1;
 
-	/* append pid and check if dir exists */
-	pr_len = sprintf((cmd_str + cmd_len), "%d", pid);
+	/* append $pid/status and check if file exists */
+	pr_len = sprintf((path_str + path_len), "%d/status", pid);
 	if (pr_len <= 0)
 		goto err;
-	cmd_len += pr_len;
-	dir = opendir(cmd_str);
-	if (!dir) {
+	path_len += pr_len;
+	fp = fopen(path_str, "r");
+	if (!fp) {
 		if (errno != ENOENT)
 			goto err;
 		else
 			return PROC_DEAD;
-	} else {
-		closedir(dir);
 	}
 
-	/* insert 'cat ' */
-	pr_len = sizeof("cat ") - 1;
-	memmove(cmd_str + pr_len, cmd_str, cmd_len);
-	pr_len = sprintf(cmd_str, "cat %s", (cmd_str + pr_len));
-        if (pr_len <= 0)
-		goto err;
-	cmd_len = pr_len;
+	/* read process name and status */
+	while (getline(&line, &alloc_len, fp) != -1) {
+		if (lnr == 0 && alloc_len > sizeof("Name:\t")) {
+			char *pname = NULL;
+			/* remove "Name:\t" prefix and trailing newline */
+			pname = &line[sizeof("Name:\t") - 1];
+			pname[strlen(pname) - 1] = '\0';
+			if (proc_name && strncmp(pname, proc_name, 15) != 0)
+				return PROC_WRONG;
+		} else if (lnr == 1 && alloc_len > sizeof("State:\t")) {
+			status = line[sizeof("State:\t") - 1];
+		} else {
+			break;
+		}
+		lnr++;
+	}
+	if (line)
+		free(line);
+	fclose(fp);
 
-	/* check process name from 'status' file */
-	pr_len = sprintf((cmd_str + cmd_len),
-		"/status | sed -n 1p | sed \'s/Name:	//g\' | tr -d \'\n\'");
-	if (pr_len <= 0)
-		goto err;
-	cmd = cmd_str;
-
-	/* run the shell cmd */
-	if (run_cmd_pipe(cmd, NULL, pbuf, sizeof(pbuf)) <= 0)
-		goto err;
-	pname = pbuf;
-	if (proc_name && strncmp(pname, proc_name, 15) != 0)
-		return PROC_WRONG;
-
-	/* remove '/status' + shell cmds, append '/status' + shell cmds */
-	memset(cmd_str + cmd_len, 0, pr_len);
-	pr_len = sprintf((cmd_str + cmd_len),
-		"/status | sed -n 2p | sed \'s/State:	//g\'");
-	if (pr_len <= 0)
-		goto err;
-	cmd = cmd_str;
-
-	/* reset pipe buffer and run the shell cmd */
-	memset(pbuf, 0, sizeof(pbuf));
-	if (run_cmd_pipe(cmd, NULL, pbuf, sizeof(pbuf)) <= 0)
+	if (status < 'A' || status > 'Z')
 		goto err;
 
 	/* zombies are not running - parent doesn't wait */
-	if (pbuf[0] == 'Z')
+	if (status == 'Z' || status == 'X')
 		return PROC_ZOMBIE;
 	return PROC_RUNNING;
 err:

commit 59c1dd8b3ae4ab506f8cd46fcecaba5613596b84

system: check_process: Fix for kernel >= 4.7 and leaks
With kernel >= 4.7 there is a line "Umask:	0022" between the
"Name:\t" and the "State:\t" line. That breaks the line number based
assumptions. So do string compares instead and break out of the loop
as soon as the process state is read.
Furthermore, if the process name is wrong, then this leaks the line
allocation and the file pointer. So store the state PROC_WRONG in
a temporary variable, break out of the loop and check the temporary
variable again to return PROC_WRONG when the line is freed and the
file is closed.

Fixes: 34c2cd745593 ("system: Optimize check_process()")

src/lib/system_l.c
@@ -74,10 +74,10 @@ enum pstate check_process (pid_t pid, const char *proc_name)
 	FILE *fp = NULL;
 	char *line = NULL;
 	size_t alloc_len = 0;
-	u32 lnr = 0;
 	char status = '\0';
 	char path_str[128] = "/proc/";
 	i32 pr_len, path_len = sizeof("/proc/") - 1;
+	enum pstate ret = PROC_ERR;
 
 	/* append $pid/status and check if file exists */
 	pr_len = sprintf((path_str + path_len), "%d/status", pid);
@@ -94,24 +94,28 @@ enum pstate check_process (pid_t pid, const char *proc_name)
 
 	/* read process name and status */
 	while (getline(&line, &alloc_len, fp) != -1) {
-		if (lnr == 0 && alloc_len > sizeof("Name:\t")) {
+		if (alloc_len > sizeof("Name:\t") &&
+		    strncmp(line, "Name:\t", sizeof("Name:\t") - 1) == 0) {
 			char *pname = NULL;
 			/* remove "Name:\t" prefix and trailing newline */
 			pname = &line[sizeof("Name:\t") - 1];
 			pname[strlen(pname) - 1] = '\0';
-			if (proc_name && strncmp(pname, proc_name, 15) != 0)
-				return PROC_WRONG;
-		} else if (lnr == 1 && alloc_len > sizeof("State:\t")) {
+			if (proc_name && strncmp(pname, proc_name, 15) != 0) {
+				ret = PROC_WRONG;
+				break;
+			}
+		} else if (alloc_len > sizeof("State:\t") &&
+		    strncmp(line, "State:\t", sizeof("State:\t") - 1) == 0) {
 			status = line[sizeof("State:\t") - 1];
-		} else {
 			break;
 		}
-		lnr++;
 	}
 	if (line)
 		free(line);
 	fclose(fp);
 
+	if (ret == PROC_WRONG)
+		return ret;
 	if (status < 'A' || status > 'Z')
 		goto err;
 