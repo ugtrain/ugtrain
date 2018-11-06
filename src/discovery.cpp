ugtrain/ugtrain

commit 16fefe650470ecb386339d18e6ab5c6ab6db1a1f

discovery: Drop the heap start/offset
There is no value in having the heap start or the heap offset
during discovery. It does not work properly any way recently. So
drop all that.

src/discovery.cpp
@@ -155,7 +155,6 @@ static void process_disc1234_malloc (MF_PARAMS)
 		cout << "m0x" << hex << mem_addr << dec << ";" << "s"
 		     << mem_size << " contains 0x" << hex << in_addr
 		     << ", offs: 0x" << in_addr - mem_addr
-		     << ", heap offs: 0x" << mem_addr - heap_start
 		     << dec << endl;
 
 		switch (stage) {

src/fifoparser.cpp
@@ -27,7 +27,6 @@ ssize_t parse_dynmem_buf (list<CfgEntry> *cfg, void *argp, char *ibuf,
 			  ssize_t *ilen, ssize_t tmp_ilen, i32 pmask,
 			  bool reverse, ptr_t code_offs, struct parse_cb *pcb)
 {
-	static ptr_t heap_start = 0;
 	ptr_t mem_addr = 0, code_addr = 0, stack_offs = 0, stack_end = 0;
 	ulong mem_size = 0;
 	char *msg_start = ibuf, *msg_end = ibuf, *pstart = ibuf, *sep_pos = ibuf;
@@ -114,7 +113,7 @@ skip_o:
 		pp.argp = argp;
 
 		// call post parsing function
-		pcb->mf(cfg, &pp, heap_start, mem_addr, mem_size,
+		pcb->mf(cfg, &pp, mem_addr, mem_size,
 			code_offs, code_addr, stack_offs);
 		break;
 	case 'f':
@@ -138,11 +137,6 @@ skip_o:
 
 		// call post parsing function
 		pcb->lf(argp, &lib_name);
-		break;
-	case 'h':
-		pstart++;
-		if (sscanf(pstart, SCN_PTR, &heap_start) != 1)
-			goto parse_err;
 		break;
 	case 'S':
 		if (!pcb || !pcb->sf)
@@ -184,32 +178,15 @@ parse_err:
 ssize_t read_dynmem_buf (list<CfgEntry> *cfg, void *argp, i32 ifd, i32 pmask,
 			 bool reverse, ptr_t code_offs, struct parse_cb *pcb)
 {
-	static ptr_t heap_start = 0;
 	static ssize_t ilen = 0;
 	ssize_t tmp_ilen;
 	static char ibuf[PIPE_BUF] = { 0 };
 	size_t max_read = sizeof(ibuf) - 1 - ilen;   // always '\0' at end
 	char *istart = ibuf + ilen;
-	char scan_ch;
 	static off_t fd_offs = REVERSE_FST;
 
 	if (reverse) {
 		if (fd_offs == REVERSE_FST) {
-			// read heap start from file first
-			tmp_ilen = read(ifd, ibuf, max_read);
-			if (tmp_ilen <= 0) {
-				tmp_ilen = 0;
-				if (argp)
-					return -1;
-			}
-			if (sscanf(ibuf, "%c", &scan_ch) != 1)
-				goto parse_err;
-			if (scan_ch == 'h') {
-				if (sscanf(ibuf + 1, SCN_PTR, &heap_start) != 1)
-					goto parse_err;
-			}
-			// clear buffer again
-			memset(ibuf, 0, tmp_ilen);
 			// set file pointer to file end
 			fd_offs = lseek(ifd, 0, SEEK_END);
 		}
@@ -244,10 +221,4 @@ ssize_t read_dynmem_buf (list<CfgEntry> *cfg, void *argp, i32 ifd, i32 pmask,
 		return -1;
 
 	return tmp_ilen;
-parse_err:
-	ugerr << "parse error at ppos: 0" << endl;
-	cerr << ibuf;
-	memset(ibuf, 0, sizeof(ibuf));
-	ilen = 0;
-	return 0;
 }

src/fifoparser.h
@@ -73,7 +73,7 @@ struct post_parse {
 };
 
 #define MF_PARAMS \
-	list<CfgEntry> *cfg, struct post_parse *pp, ptr_t heap_start, \
+	list<CfgEntry> *cfg, struct post_parse *pp, \
 	ptr_t mem_addr, size_t mem_size, ptr_t code_offs, ptr_t code_addr, \
 	ptr_t stack_offs
 

commit e42ee24b428f6f404a2ff72d5256d1b802584bce

discovery: Read the stack end in reverse mode first
Dropping the heap start handling has the side effect that the stack
end (now in the first line) is not read any more during reverse
parsing for discovery. Handling the stack end at the end is also
always a problem - especially if many mallocs are recorded as well.

So partially revert that commit and read as well as handle the stack
end first.

Fixes: 16fefe650470 ("discovery: Drop the heap start/offset")

src/discovery.cpp
@@ -514,7 +514,7 @@ i32 prepare_discovery (Options *opt)
 	stage = disc_part[0];
 	switch (stage) {
 	case '0':
-		// just get stack end and heap start
+		// just get stack end
 		*opt->disc_str = "0";
 		opt->disc_offs = 0;
 		break;

src/fifoparser.cpp
@@ -183,10 +183,31 @@ ssize_t read_dynmem_buf (list<CfgEntry> *cfg, void *argp, i32 ifd, i32 pmask,
 	static char ibuf[PIPE_BUF] = { 0 };
 	size_t max_read = sizeof(ibuf) - 1 - ilen;   // always '\0' at end
 	char *istart = ibuf + ilen;
+	char scan_ch;
+	ptr_t stack_end = 0;
 	static off_t fd_offs = REVERSE_FST;
 
 	if (reverse) {
 		if (fd_offs == REVERSE_FST) {
+			// read stack end from file first
+			tmp_ilen = read(ifd, ibuf, max_read);
+			if (tmp_ilen <= 0) {
+				tmp_ilen = 0;
+				if (argp)
+					return -1;
+			}
+			if (sscanf(ibuf, "%c", &scan_ch) != 1)
+				goto parse_err;
+			if (scan_ch == 'S') {
+				if (sscanf(ibuf + 1, SCN_PTR, &stack_end) != 1)
+					goto parse_err;
+			}
+			if (pcb && pcb->sf && stack_end) {
+				// call post parsing function
+				pcb->sf(cfg, argp, stack_end);
+			}
+			// clear buffer again
+			memset(ibuf, 0, tmp_ilen);
 			// set file pointer to file end
 			fd_offs = lseek(ifd, 0, SEEK_END);
 		}
@@ -221,4 +242,10 @@ ssize_t read_dynmem_buf (list<CfgEntry> *cfg, void *argp, i32 ifd, i32 pmask,
 		return -1;
 
 	return tmp_ilen;
+parse_err:
+	ugerr << "parse error at ppos: 0" << endl;
+	cerr << ibuf;
+	memset(ibuf, 0, sizeof(ibuf));
+	ilen = 0;
+	return 0;
 }