ugtrain/ugtrain

commit 0a8bb9b2feafad12a9a59c2f38093eefe1e38392

ugtrain: Use C++ strings for lib_name
C++ does the basic memory management for us when using C++ strings
instead of C strings.

src/aslr.cpp
@@ -61,12 +61,12 @@ static inline i32 send_bt_addrs (struct region_map *map, void *data)
 	struct lf_disc_params *lfp = (struct lf_disc_params *) data;
 	string *disc_lib = lfp->disc_lib;
 	i32 ofd = lfp->ofd;
-	char *lib_name = basename(map->file_path);
+	string lib_name = string(basename(map->file_path));
 	ptr_t lib_start, lib_end;
 
 	if (map->exec != 'x')
 		goto out;
-	if (!strstr(lib_name, disc_lib->c_str()))
+	if (lib_name != *disc_lib)
 		goto out;
 
 	lib_start = (ptr_t) map->start;
@@ -86,10 +86,10 @@ static void handle_disc_pic (LF_PARAMS)
 	string *disc_lib = lfp->disc_lib;
 	i32 ret;
 
-	//ugout << lib_name << " loaded" << endl;
+	//ugout << *lib_name << " loaded" << endl;
 
 	if (!disc_lib->empty() && disc_lib->at(0) != '\1' &&
-	    strstr(lib_name, disc_lib->c_str())) {
+	    *lib_name == *disc_lib) {
 		ret = read_maps(pid, send_bt_addrs, lfp);
 		if (!ret)
 			send_lib_bounds(ofd, 0, UINTPTR_MAX);
@@ -168,17 +168,17 @@ void do_disc_pic_work (pid_t pid, Options *opt,
  * Libraries are always built as position-independent code (PIC).
  * So we have to find the start and the end of their code regions.
  */
-static inline void find_lib_region (struct list_head *rlist, const char *lib,
+static inline void find_lib_region (struct list_head *rlist, const string *lib,
 				    ptr_t *lib_start, ptr_t *lib_end)
 {
 	struct region *it;
 
 	clist_for_each_entry (it, rlist, list) {
-		char *file_name;
+		string file_name;
 		if (!it->flags.exec || it->type == REGION_TYPE_EXE)
 		       continue;
-		file_name = basename(it->file_path);
-		if (strstr(file_name, lib)) {
+		file_name = string(basename(it->file_path));
+		if (file_name == *lib) {
 			*lib_start = (ptr_t) it->start;
 			*lib_end = (ptr_t) (it->start + it->size);
 			return;
@@ -216,9 +216,9 @@ void get_lib_load_addr (LF_PARAMS)
 			continue;
 		grow = dynmem->grow;
 		old_dynmem = dynmem;
-		if (dynmem->lib && strstr(dynmem->lib, lib_name))
+		if (dynmem->lib && *dynmem->lib == *lib_name)
 			dynmem->code_addr = dynmem->code_offs + lib_start;
-		if (grow && grow->lib && strstr(grow->lib, lib_name))
+		if (grow && grow->lib && *grow->lib == *lib_name)
 			grow->code_addr = grow->code_offs + lib_start;
 	}
 	send_lib_bounds(ofd, lib_start, lib_end);
@@ -310,7 +310,7 @@ void handle_aslr (Options *opt, list<CfgEntry> *cfg, i32 ifd,
 		} else {                          // early PIC
 			ptr_t lib_start, lib_end;
 
-			find_lib_region(rlist, opt->disc_lib->c_str(), &lib_start,
+			find_lib_region(rlist, opt->disc_lib, &lib_start,
 				&lib_end);
 			// notify libmemdisc that we are ready for PIC handling
 			// and send backtrace filter for early library load

src/cfgentry.h
@@ -55,7 +55,7 @@ public:
 	ptr_t code_addr;
 	ptr_t code_offs;              // for late PIC support
 	ptr_t stack_offs;
-	char *lib;                    // for PIC support
+	string *lib;                  // for PIC support
 
 	// later determined values
 	vector<size_t> v_msize;       // set by malloc/realloc calls
@@ -70,7 +70,7 @@ public:
 	ptr_t code_addr;
 	ptr_t code_offs;              // for late PIC support
 	ptr_t stack_offs;
-	char *lib;                    // for PIC support
+	string *lib;                  // for PIC support
 	GrowEntry *grow;
 
 	list<CfgEntry*> cfg_act;
@@ -133,7 +133,7 @@ typedef enum {
 } check_e;
 
 struct type {
-	char *lib_name;
+	string *lib_name;
 	LibEntry *lib;
 	bool on_stack;
 	bool is_signed;

src/cfgoutput.cpp
@@ -101,8 +101,8 @@ static void output_config_en (CfgEntry *cfg_en, const char *pfx)
 		ugout << pfx << "  " << cfg_en->name << " ";
 		if (cfg_en->type.on_stack)
 			cout << "stack 0x";
-		else if (cfg_en->type.lib_name)
-			cout << "lib " << cfg_en->type.lib_name << " 0x";
+		else if (cfg_en->type.lib_name && !cfg_en->type.lib_name->empty())
+			cout << "lib " << *cfg_en->type.lib_name << " 0x";
 		else
 			cout << "0x";
 		cout << hex << cfg_en->addr << dec
@@ -163,8 +163,8 @@ static void output_checks (CfgEntry *cfg_en)
 		ugout << "    check ";
 		if (cfg_en->type.on_stack)
 			cout << "stack 0x";
-		else if (cfg_en->type.lib_name)
-			cout << "lib " << cfg_en->type.lib_name << " 0x";
+		else if (cfg_en->type.lib_name && !cfg_en->type.lib_name->empty())
+			cout << "lib " << *cfg_en->type.lib_name << " 0x";
 		else
 			cout << "0x";
 		cout << hex << cfg_en->addr << dec << check_op;
@@ -182,8 +182,8 @@ static void output_checks (CfgEntry *cfg_en)
 		}
 		if (it->type.on_stack)
 			cout << "stack 0x";
-		else if (it->type.lib_name)
-			cout << "lib " << it->type.lib_name << " 0x";
+		else if (it->type.lib_name && !it->type.lib_name->empty())
+			cout << "lib " << *it->type.lib_name << " 0x";
 		else
 			cout << "0x";
 		cout  << hex << it->addr << dec;
@@ -272,16 +272,16 @@ void output_config (Options *opt, list<CfgEntry> *cfg)
 			      << dynmem->mem_size << " 0x" << hex
 			      << dynmem->code_addr << " 0x" << dynmem->stack_offs
 			      << dec;
-			if (dynmem->lib)
-				cout << " " << dynmem->lib;
+			if (dynmem->lib && !dynmem->lib->empty())
+				cout << " " << *dynmem->lib;
 			if (grow) {
 				cout << " growing " << grow->size_min
 				     << " " << grow->size_max << " ";
 				output_grow_method(grow);
 				cout << " 0x" << hex << grow->code_addr << " 0x"
 				     << grow->stack_offs << dec;
-				if (grow->lib)
-					cout << " " << grow->lib;
+				if (grow->lib && !grow->lib->empty())
+					cout << " " << *grow->lib;
 			}
 			OUTPUT_CACHE(dynmem);
 			cout << endl;

src/cfgparser.cpp
@@ -351,16 +351,10 @@ static bool parse_obj_num_chk (list<CfgEntry> *cfg, CheckEntry *chk_en,
 
 static inline ptr_t get_cfg_en_addr (CheckEntry *chk_en, CfgEntry *cfg_en)
 {
-	string tmp_str;
-
-	if (cfg_en->type.on_stack) {
+	if (cfg_en->type.on_stack)
 		chk_en->type.on_stack = true;
-	} else if (cfg_en->type.lib_name) {
-		if (chk_en->type.lib_name)
-			delete[] chk_en->type.lib_name;
-		tmp_str = cfg_en->type.lib_name;
-		chk_en->type.lib_name = to_c_str(&tmp_str);
-	}
+	else if (cfg_en->type.lib_name && !cfg_en->type.lib_name->empty())
+		chk_en->type.lib_name = new string(*cfg_en->type.lib_name);
 	return cfg_en->addr;
 }
 
@@ -401,9 +395,9 @@ start:
 				cfg_parse_err(line, lnr, lidx);
 			tmp_str = parse_value_name(line, lnr, start, false, NULL);
 			if (chk_en)
-				chk_en->type.lib_name = to_c_str(&tmp_str);
+				chk_en->type.lib_name = new string(tmp_str);
 			else
-				cfg_en->type.lib_name = to_c_str(&tmp_str);
+				cfg_en->type.lib_name = new string(tmp_str);
 			cfg = NULL; cfg_en = NULL; chk_en = NULL;
 			goto start;
 		}
@@ -703,10 +697,10 @@ static void parse_key_bindings (string *line, u32 lnr, u32 *start,
  * returns the string at the end of a line
  * returns NULL if there is no string, it is "exe" or contains blanks
  */
-static inline char *parse_pic_lib (string *line, u32 *start)
+static inline string parse_pic_lib (string *line, u32 *start)
 {
 	string tmp_str;
-	char *lib = NULL;
+	string lib;
 
 	if (*start >= line->length())
 		goto out;
@@ -715,7 +709,7 @@ static inline char *parse_pic_lib (string *line, u32 *start)
 	if (tmp_str == "exe" || tmp_str.find(' ') != string::npos ||
 	    tmp_str.find('\t') != string::npos)
 		goto out;
-	lib = to_c_str(&tmp_str);
+	lib = tmp_str;
 out:
 	return lib;
 }
@@ -755,7 +749,7 @@ static void parse_growing (DynMemEntry *dynmem_enp, string *line, u32 lnr,
 	grow_enp->code_offs = grow_enp->code_addr;
 	grow_enp->stack_offs =
 		parse_address(NULL, NULL, NULL, line, lnr, start);
-	grow_enp->lib = parse_pic_lib(line, start);
+	grow_enp->lib = new string(parse_pic_lib(line, start));
 	grow_enp->v_msize.clear();
 	dynmem_enp->grow = grow_enp;
 }
@@ -780,7 +774,7 @@ static void parse_dynmem (DynMemEntry *dynmem_enp, bool from_grow, string *line,
 		dynmem_enp->stack_offs =
 			parse_address(NULL, NULL, NULL, line, lnr, start);
 		dynmem_enp->cfg_line = lnr;
-		dynmem_enp->lib = parse_pic_lib(line, start);
+		dynmem_enp->lib = new string(parse_pic_lib(line, start));
 	}
 	init_cache(&cache, PTR_MAX);
 	dynmem_enp->cache_list = new list<CacheEntry>;
@@ -835,14 +829,14 @@ do {									\
 	LibEntry lib_en;						\
 									\
 	list_for_each (opt->lib_list, lit) {				\
-		if (lit->name != entry.type.lib_name)			\
+		if (lit->name != *entry.type.lib_name)			\
 			continue;					\
 		entry.type.lib = &(*lit);				\
 		found = true;						\
 		break;							\
 	}								\
 	if (!found) {							\
-		lib_en.name = entry.type.lib_name;			\
+		lib_en.name = *entry.type.lib_name;			\
 		lib_en.start = 0;					\
 		lib_en.is_loaded = false;				\
 		lib_en.skip_val = false;				\
@@ -940,7 +934,7 @@ void read_config (Options *opt, vector<string> *cfg_lines)
 				if (in_dynmem || in_ptrmem)
 					cfg_parse_err(&line, lnr, start);
 				opt->val_on_stack = true;
-			} else if (chk_en.type.lib_name) {
+			} else if (chk_en.type.lib_name && !chk_en.type.lib_name->empty()) {
 				if (in_dynmem || in_ptrmem)
 					cfg_parse_err(&line, lnr, start);
 				FIND_ADD_LIB_ENTRY(chk_en);
@@ -1141,7 +1135,7 @@ void read_config (Options *opt, vector<string> *cfg_lines)
 				if (in_dynmem || in_ptrmem)
 					cfg_parse_err(&line, lnr, start);
 				opt->val_on_stack = true;
-			} else if (cfg_en.type.lib_name) {
+			} else if (cfg_en.type.lib_name && !cfg_en.type.lib_name->empty()) {
 				if (in_dynmem || in_ptrmem)
 					cfg_parse_err(&line, lnr, start);
 				FIND_ADD_LIB_ENTRY(cfg_en);

src/fifoparser.cpp
@@ -31,7 +31,7 @@ ssize_t parse_dynmem_buf (list<CfgEntry> *cfg, void *argp, char *ibuf,
 	ptr_t mem_addr = 0, code_addr = 0, stack_offs = 0, stack_end = 0;
 	ulong mem_size = 0;
 	char *msg_start = ibuf, *msg_end = ibuf, *pstart = ibuf, *sep_pos = ibuf;
-	char *lib_name = NULL;
+	string lib_name;
 	struct post_parse pp;
 	char scan_ch;
 
@@ -134,13 +134,10 @@ skip_o:
 		if (*pstart != ';')
 			goto parse_err;
 		pstart++;
-		lib_name = new char[msg_end - pstart + 1];
-		memcpy(lib_name, pstart, msg_end - pstart);
-		lib_name[msg_end - pstart] = '\0';
+		lib_name = string(pstart, msg_end - pstart);
 
 		// call post parsing function
-		pcb->lf(argp, lib_name);
-		delete[] lib_name;
+		pcb->lf(argp, &lib_name);
 		break;
 	case 'h':
 		pstart++;

src/fifoparser.h
@@ -81,7 +81,7 @@ struct post_parse {
 	list<CfgEntry> *cfg, void *argp, ptr_t mem_addr
 
 #define LF_PARAMS \
-	void *argp, char *lib_name
+	void *argp, string *lib_name
 
 #define SF_PARAMS \
 	list<CfgEntry> *cfg, void *argp, ptr_t stack_end

src/ugtrain.cpp
@@ -879,7 +879,7 @@ static i32 prepare_dynmem (Options *opt, list<CfgEntry> *cfg,
 				dynmem->stack_offs);
 			if (dynmem->lib)
 				pos += snprintf(obuf + pos, sizeof(obuf) - pos,
-					";%s", dynmem->lib);
+					";%s", dynmem->lib->c_str());
 			else
 				pos += snprintf(obuf + pos, sizeof(obuf) - pos,
 						";exe");
@@ -893,7 +893,7 @@ static i32 prepare_dynmem (Options *opt, list<CfgEntry> *cfg,
 				if (grow->lib)
 					pos += snprintf(obuf + pos,
 						sizeof(obuf) - pos,
-						";%s", grow->lib);
+						";%s", grow->lib->c_str());
 				else
 					pos += snprintf(obuf + pos,
 						sizeof(obuf) - pos, ";exe");

src/util.cpp
@@ -101,7 +101,7 @@ void clear_config (void)
 		if (it->type.is_cstrp)
 			free(it->cstr);
 		if (it->type.lib_name)
-			delete[] it->type.lib_name;
+			delete it->type.lib_name;
 	}
 	// Clean up dynamic/pointer memory config
 	list_for_each (cfg, it) {
@@ -109,7 +109,7 @@ void clear_config (void)
 		PtrMemEntry *ptrmem = it->ptrmem;
 		if (dynmem)
 			CLEANUP_MEM(dynmem,
-				if (dynmem->lib) delete[] dynmem->lib);
+				if (dynmem->lib) delete dynmem->lib);
 		else if (ptrmem)
 			CLEANUP_MEM(ptrmem, ;);
 		// Clean up checks
@@ -117,7 +117,7 @@ void clear_config (void)
 			list<CheckEntry>::iterator chk_it;
 			list_for_each (it->checks, chk_it) {
 				if (chk_it->type.lib_name)
-					delete[] chk_it->type.lib_name;
+					delete chk_it->type.lib_name;
 			}
 			it->checks->clear();
 			delete it->checks;

commit 810ee3402ed46d4f239e2b2b13ad485ece659d7c

aslr: Fix the PIE handling
Tests on Ubuntu Bionic have shown that the commit "ugtrain: Use C++
strings for lib_name" did not cover all occurrences of dynmem->lib
and dynmem->grow->lib. This causes that offset 0x0 is sent to
libmemhack instead of the actual exe offset.
So fix the PIE handling by checking for dynmem->lib->empty() and
grow->lib->empty() as well.

Fixes: 0a8bb9b2feaf ("ugtrain: Use C++ strings for lib_name")

src/aslr.cpp
@@ -327,7 +327,7 @@ void handle_aslr (Options *opt, list<CfgEntry> *cfg, i32 ifd,
 				continue;
 			grow = dynmem->grow;
 			old_dynmem = dynmem;
-			if (!dynmem->lib)
+			if (!dynmem->lib || dynmem->lib->empty())
 				code_offs = exe_offs;
 			else
 				find_lib_region(rlist, dynmem->lib, &code_offs,
@@ -336,7 +336,7 @@ void handle_aslr (Options *opt, list<CfgEntry> *cfg, i32 ifd,
 				PRI_PTR ";", code_offs);
 			dynmem->code_addr += code_offs;
 			if (grow) {
-				if (!grow->lib)
+				if (!grow->lib || grow->lib->empty())
 					code_offs = exe_offs;
 				else
 					find_lib_region(rlist, grow->lib,