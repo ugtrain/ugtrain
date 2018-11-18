ugtrain/ugtrain

commit 45029cacbc2a8aad1f65473800053128dd462cbb

ugtrain: Use C++ discovery strings and drop stage 5
The discovery strings opt->disc_str and opt->disc_lib are changed
at many locations. The cleanup would be too cumbersome as C strings.
So convert those to C++ strings and let C++ do the dirty work
regarding memory handling. Clean them up during options cleanup. As
a C++ string does not accept getting "\0" assigned, change the value
representing disc_lib "exe" to "\1".

Also drop discovery stage 5 remainings. Adaptation should load the
libmemhack automatically. So change do_assumptions() accordingly.
Rework the help a bit to simplify things and to reflect this.

src/discovery.cpp
@@ -138,7 +138,7 @@ static void process_disc1234_malloc (MF_PARAMS)
 	ptr_t in_addr = dpp->in_addr;
 	Options *opt = dpp->opt;
 	struct list_head *rlist = dpp->rlist;
-	char stage = opt->disc_str[0];
+	char stage = opt->disc_str->at(0);
 	ptr_t codes[MAX_BT] = { 0 };
 	ptr_t soffs[MAX_BT] = { 0 };
 	char *sep_pos;
@@ -306,7 +306,7 @@ err:
 
 i32 postproc_discovery (Options *opt)
 {
-	if (opt->disc_str[0] >= '1' && opt->disc_str[0] <= '4')
+	if (opt->disc_str->at(0) >= '1' && opt->disc_str->at(0) <= '4')
 		return postproc_stage1234(opt);
 	return -1;
 }
@@ -392,9 +392,9 @@ void process_discovery (Options *opt, i32 ifd, i32 dfd, i32 ofd, i32 pid)
 	struct list_head *rlist = opt->rlist;
 	pid_t worker_pid;
 
-	if (opt->disc_str[0] == 'p')
+	if (opt->disc_str->at(0) == 'p')
 		opt->disc_str[0] = opt->disc_str[opt->disc_offs];
-	if (opt->disc_str[0] == '0') {
+	if (opt->disc_str->at(0) == '0') {
 		if (opt->scanmem_pid > 0) {
 			wait_orphan(pid, opt->proc_name);
 			wait_proc(opt->scanmem_pid);
@@ -403,14 +403,14 @@ void process_discovery (Options *opt, i32 ifd, i32 dfd, i32 ofd, i32 pid)
 			wait_orphan(pid, opt->proc_name);
 		}
 		exit(0);
-	} else if (opt->disc_str[0] >= '1' && opt->disc_str[0] <= '4') {
+	} else if (opt->disc_str->at(0) >= '1' && opt->disc_str->at(0) <= '4') {
 		struct disc_params params = { dfd, pid, opt };
 		get_stack_start(opt, pid, rlist);
-		if (opt->disc_str[0] >= '3' || opt->disc_offs > 0)
+		if (opt->disc_str->at(0) >= '3' || opt->disc_offs > 0)
 			handle_aslr(opt, cfg, ifd, ofd, pid, rlist);
 		worker_pid = fork_proc(run_stage1234_loop, &params);
-		if (opt->disc_str[0] >= '3' && opt->disc_lib &&
-		    opt->disc_lib[0] != '\0')
+		if (opt->disc_str->at(0) >= '3' && !opt->disc_lib->empty() &&
+		    opt->disc_lib->at(0) != '\1')
 			do_disc_pic_work(pid, opt, ifd, ofd, rlist);
 		else
 			wait_orphan(pid, opt->proc_name);
@@ -435,7 +435,7 @@ static inline void stage34_args_err (char stage)
 
 // Does the user request to filter the backtrace to the given lib?
 // returns 0 for success, -1 for error
-static inline i32 parse_bt_filter (char **disc_part, char **disc_lib,
+static inline i32 parse_bt_filter (char **disc_part, string *disc_lib,
 				   char stage)
 {
 	i32 ret = -1;
@@ -454,12 +454,10 @@ static inline i32 parse_bt_filter (char **disc_part, char **disc_lib,
 #if (DISC_DEBUG)
 	ugout << "PIC: filtering backtrace to " << *disc_part << endl;
 #endif
-	if (strncmp(*disc_part, "exe", sizeof("exe") - 1) == 0) {
-		*disc_lib = (char *) "\0";
-	} else {
-		*disc_lib = new char[strlen(*disc_part) + 1];
-		strcpy(*disc_lib, *disc_part);
-	}
+	if (strncmp(*disc_part, "exe", sizeof("exe") - 1) == 0)
+		*disc_lib = "\1";
+	else
+		*disc_lib = *disc_part;
 	*pos = ';';
 	*disc_part = pos;
 success:
@@ -470,31 +468,33 @@ out:
 
 i32 prepare_discovery (Options *opt)
 {
-	string disc_str, cmd_str;
-	char *pos, *disc_part;
+	string cmd_str;
+	char *pos, *disc_part, *disc_str;
 	char stage;
 	i32 ret;
 	list<CfgEntry>::iterator it;
 	ptr_t code_addr = 0;
 	ulong mem_size = 0;
 
-	if (!opt->disc_str)
+	if (opt->disc_str->empty())
 		return 0;
 
-	if (opt->disc_str[0] == 'p') {
-		disc_part = opt->disc_str + 1;
+	disc_str = (char *) opt->disc_str->c_str();
+
+	if (disc_str[0] == 'p') {
+		disc_part = disc_str + 1;
 		pos = strstr(disc_part, ";;");
 		if (!pos)
 			goto err;
-		opt->disc_offs = pos - opt->disc_str + 2;
+		opt->disc_offs = pos - disc_str + 2;
 	}
 
-	disc_part = opt->disc_str + opt->disc_offs;
+	disc_part = disc_str + opt->disc_offs;
 	stage = disc_part[0];
 	switch (stage) {
 	case '0':
 		// just get stack end and heap start
-		opt->disc_str = (char *) "0";
+		*opt->disc_str = "0";
 		opt->disc_offs = 0;
 		break;
 	case '1':
@@ -502,19 +502,17 @@ i32 prepare_discovery (Options *opt)
 	case '2':
 		disc_part++;
 		if (disc_part[0] == '\0') {
-			disc_str = opt->disc_str;
-			disc_str += ";0";
-			opt->disc_str = to_c_str(&disc_str);
+			*opt->disc_str += ";0";
 		} else {
 			ret = sscanf(disc_part, ";%lu", &mem_size);
 			if (ret < 0) {
 				ugerr << "Syntax error in discovery string!"
 				      << endl;
-				ugerr << "disc_str: " << opt->disc_str << endl;
+				ugerr << "disc_str: " << *opt->disc_str << endl;
 				goto err;
 			}
 		}
-		ugout << "disc_str: " << opt->disc_str << endl;
+		ugout << "disc_str: " << *opt->disc_str << endl;
 		break;
 	case '3':
 		ret = strncmp(&disc_part[1], ";" GBT_CMD ";",
@@ -524,7 +522,7 @@ i32 prepare_discovery (Options *opt)
 		// fall through
 	case '4':
 		disc_part++;
-		if (parse_bt_filter(&disc_part, &opt->disc_lib, stage))
+		if (parse_bt_filter(&disc_part, opt->disc_lib, stage))
 			goto err;
 		ret = sscanf(disc_part, ";%lu;" SCN_PTR, &mem_size, &code_addr);
 		if (ret < 1) {
@@ -537,7 +535,7 @@ i32 prepare_discovery (Options *opt)
 				 "size first!" << endl;
 			goto err;
 		}
-		ugout << "disc_str: " << opt->disc_str << endl;
+		ugout << "disc_str: " << *opt->disc_str << endl;
 		break;
 	default:
 		goto err;
@@ -555,17 +553,17 @@ check_beginner_stage4 (Options *opt)
 	i32 ret;
 	ulong mem_size;
 
-	ret = sscanf(opt->disc_str, "%lu", &mem_size);
+	ret = sscanf(opt->disc_str->c_str(), "%lu", &mem_size);
 	if (ret == 1 && mem_size >= 8) {
 		u32 i;
 		string disc_str;
-		for (i = 0; i < strlen(opt->disc_str); i++) {
-			if (!isdigit(opt->disc_str[i]))
+		for (i = 0; i < opt->disc_str->length(); i++) {
+			if (!isdigit(opt->disc_str->c_str()[i]))
 				goto err;
 		}
 		disc_str = "4;";
-		disc_str += opt->disc_str;
-		opt->disc_str = to_c_str(&disc_str);
+		disc_str += *opt->disc_str;
+		*opt->disc_str = disc_str;
 	}
 	return 0;
 err:
@@ -579,10 +577,10 @@ bool init_discovery (Options *opt)
 
 	if (check_beginner_stage4(opt) != 0)
 		exit(-1);
-	if (opt->disc_str[0] >= '0' && opt->disc_str[0] <= '4') {
+	if (opt->disc_str->at(0) >= '0' && opt->disc_str->at(0) <= '4') {
 		if (opt->run_scanmem && !tool_is_available((char *) "scanmem"))
 			exit(-1);
-		if (opt->disc_str[0] >= '3') {
+		if (opt->disc_str->at(0) >= '3') {
 			opt->have_objdump = tool_is_available(
 				(char *) "objdump");
 			if (!opt->have_objdump)

src/adaption.cpp
@@ -83,7 +83,7 @@ static i32 take_over_config (Options *opt, vector<string> *cfg_lines)
 
 	// Run game with libmemhack
 	opt->do_adapt = false;
-	opt->disc_str = NULL;
+	opt->disc_str->clear();
 	use_libmemhack(opt);
 
 	return ret;
@@ -285,7 +285,7 @@ i32 process_adaptation (Options *opt, vector<string> *cfg_lines)
 	char ch;
 	i32 ret = 0;
 
-	if (opt->adp_required && !opt->do_adapt && !opt->disc_str &&
+	if (opt->adp_required && !opt->do_adapt && opt->disc_str->empty() &&
 	    !opt->run_scanmem) {
 		if (!opt->adp_script) {
 			ugerr << "Error, adaptation required but no adaptation script!" << endl;

src/aslr.cpp
@@ -54,19 +54,19 @@ static inline void send_lib_bounds (i32 ofd, ptr_t lib_start, ptr_t lib_end)
  * sends found code addresses as the new backtrace filter to libmemdisc,
  * returns 1 for stopping the iteration (disc_lib found) and 0 otherwise
  *
- * Assumptions: disc_lib != NULL, disc_lib[0] != '\0'
+ * Assumptions: disc_lib != NULL, disc_lib not empty, disc_lib[0] != '\1'
  */
 static inline i32 send_bt_addrs (struct region_map *map, void *data)
 {
 	struct lf_disc_params *lfp = (struct lf_disc_params *) data;
-	char *disc_lib = lfp->disc_lib;
+	string *disc_lib = lfp->disc_lib;
 	i32 ofd = lfp->ofd;
 	char *lib_name = basename(map->file_path);
 	ptr_t lib_start, lib_end;
 
 	if (map->exec != 'x')
 		goto out;
-	if (!strstr(lib_name, disc_lib))
+	if (!strstr(lib_name, disc_lib->c_str()))
 		goto out;
 
 	lib_start = (ptr_t) map->start;
@@ -83,13 +83,13 @@ static void handle_disc_pic (LF_PARAMS)
 	struct lf_disc_params *lfp = (struct lf_disc_params *) argp;
 	pid_t pid = lfp->pid;
 	i32 ofd = lfp->ofd;
-	char *disc_lib = lfp->disc_lib;
+	string *disc_lib = lfp->disc_lib;
 	i32 ret;
 
 	//ugout << lib_name << " loaded" << endl;
 
-	if (disc_lib && disc_lib[0] != '\0' &&
-	    strstr(lib_name, disc_lib)) {
+	if (!disc_lib->empty() && disc_lib->at(0) != '\1' &&
+	    strstr(lib_name, disc_lib->c_str())) {
 		ret = read_maps(pid, send_bt_addrs, lfp);
 		if (!ret)
 			send_lib_bounds(ofd, 0, UINTPTR_MAX);
@@ -101,8 +101,9 @@ static void handle_disc_pic (LF_PARAMS)
  *  0: data has been read
  * -1: no data read
  */
-static inline i32 read_libs_from_fifo (pid_t pid, char *disc_lib,
-				       i32 ifd, i32 ofd)
+static inline i32
+read_libs_from_fifo (pid_t pid, string *disc_lib,
+		     i32 ifd, i32 ofd)
 {
 	ssize_t rbytes;
 	struct parse_cb pcb = { NULL };
@@ -167,7 +168,7 @@ void do_disc_pic_work (pid_t pid, Options *opt,
  * Libraries are always built as position-independent code (PIC).
  * So we have to find the start and the end of their code regions.
  */
-static inline void find_lib_region (struct list_head *rlist, char *lib,
+static inline void find_lib_region (struct list_head *rlist, const char *lib,
 				    ptr_t *lib_start, ptr_t *lib_end)
 {
 	struct region *it;
@@ -299,17 +300,17 @@ void handle_aslr (Options *opt, list<CfgEntry> *cfg, i32 ifd,
 
 	if (opt->pure_statmem)
 		return;
-	if (opt->disc_str) {
-		if (!opt->disc_lib)
+	if (!opt->disc_str->empty()) {
+		if (opt->disc_lib->empty())
 			return;
-		if (opt->disc_lib[0] == '\0') {   // PIE
+		if (opt->disc_lib->at(0) == '\1') {   // PIE
 			osize += snprintf(obuf + osize, sizeof(obuf) - osize,
 				PRI_PTR ";" PRI_PTR ";" PRI_PTR "\n", exe_start,
 				exe_end, exe_offs);
 		} else {                          // early PIC
 			ptr_t lib_start, lib_end;
 
-			find_lib_region(rlist, opt->disc_lib, &lib_start,
+			find_lib_region(rlist, opt->disc_lib->c_str(), &lib_start,
 				&lib_end);
 			// notify libmemdisc that we are ready for PIC handling
 			// and send backtrace filter for early library load

src/fifoparser.h
@@ -28,9 +28,9 @@
 
 // late PIC discovery handling parameters
 struct lf_disc_params {
-	char  *disc_lib;
-	pid_t pid;
-	i32   ofd;
+	string *disc_lib;
+	pid_t  pid;
+	i32    ofd;
 };
 
 // malloc queue to delay malloc processing

src/options.cpp
@@ -17,6 +17,7 @@
 #include <cstring>
 
 /* local includes */
+#include <common.h>
 #include <options.h>
 #include <testopts.h>
 #include <util.h>
@@ -26,35 +27,26 @@
 
 void do_assumptions (Options *opt)
 {
-	/* Adaption handling */
+	/* Adaptation handling */
 	/* '-D 1 -A' --> '-D 1', '-S -A' --> '-S' */
-	if ((opt->disc_str && opt->disc_str[0] != '5') || opt->run_scanmem)
+	if (!opt->disc_str->empty() || opt->run_scanmem)
 		opt->do_adapt = false;
-
-	if (opt->do_adapt) {
-		/* '-A' --> '-A -D 5' */
-		if (!opt->disc_str)
-			opt->disc_str = (char *) "5";
-		/* '-P libmemhack*' -> '' */
-		if (opt->preload_lib && strncmp(opt->preload_lib, LHACK_PRE,
-		    sizeof(LHACK_PRE) - 1) == 0)
-			opt->preload_lib = NULL;
-	}
 
 	/* Preloading/Starting handling */
 	if (!opt->preload_lib) {
 		/* '-D <str>' --> '-D <str> -P libmemdisc32/64.so' */
-		if (opt->disc_str) {
+		if (!opt->disc_str->empty()) {
 			use_libmemdisc(opt);
 		/* '-S' --> '-S -P libmemhack32/64.so',
+		   '-A' --> '-A -P libmemhack32/64.so',
 		   '--pre-cmd=' --> '--pre-cmd= -P libmemhack32/64.so' */
-		} else if (opt->run_scanmem || opt->pre_cmd) {
+		} else if (opt->run_scanmem || opt->pre_cmd || opt->do_adapt) {
 			use_libmemhack(opt);
 		}
 	/* '-P libmemhack32/64.so -D <str>' -->
 	   '-D <str> -P libmemdisc32/64.so' */
-	} else if (opt->disc_str && strncmp(opt->preload_lib, LHACK_PRE,
-	    sizeof(LHACK_PRE) - 1) == 0) {
+	} else if (!opt->disc_str->empty() && strncmp(opt->preload_lib,
+	    LHACK_PRE, sizeof(LHACK_PRE) - 1) == 0) {
 		use_libmemdisc(opt);
 	}
 }
@@ -71,19 +63,17 @@ PROG_NAME " is the universal elite game trainer\n"
 "--preload, -P [lib]:	start the game preloaded with the given lib -\n"
 "			without argument, \'-P " LHACK_PRE "32/64"
 				LIB_END "\' is assumed\n"
-"			(depending on \'sizeof(void *)\', ignored for root)\n"
 "			- starts the game for pure static memory as well\n"
 "--discover, -D [str]:	do discovery by sending the string to the discovery\n"
 "			library - without \'-P\', \'-P "
 				LDISC_PRE "32/64" LIB_END "\'\n"
-"			is assumed depending on \'sizeof(void *)\'\n"
+"			is assumed\n"
 "--scanmem, -S:		run \'scanmem\' without/together with \'-D\' in the\n"
 "			same process group as the game (avoid root access)\n"
 "			- assumes \'-P\' and ignores the config\n"
-"--adapt, -A:		run the adaption script from the config to determine\n"
-"			malloc code addresses and discover the stack offsets\n"
-"			- assumes \'-D 5\' (stage 5: finding stack offset)\n"
-"			using the new code address (rejects if root)\n"
+"--adapt, -A:		run the adaptation script from the config to determine\n"
+"			the new malloc code addresses and start the game with\n"
+"			the \'-P\' option\n"
 "Misc Options\n"
 "--pre-cmd=<str>:	together with the assumed \'-P\' it is possible to\n"
 "			specify a preloader command\n"
@@ -169,6 +159,10 @@ skip_statmem_cache:
 		delete[] opt->proc_name;
 	if (opt->adp_script)
 		delete[] opt->adp_script;
+	if (opt->disc_lib)
+		delete opt->disc_lib;
+	if (opt->disc_str)
+		delete opt->disc_str;
 }
 
 static void init_options (Options *opt)
@@ -181,6 +175,8 @@ static void init_options (Options *opt)
 	init_opt_globals(opt);
 	opt->procmem_fd = -1;
 	opt->scanmem_pid = -1;
+	opt->disc_str = new string;
+	opt->disc_lib = new string;
 	tmp_str = DYNMEM_FILE;
 	opt->dynmem_file = to_c_str(&tmp_str);
 	opt->cache_list = new list<CacheEntry>;
@@ -222,9 +218,9 @@ void parse_options (i32 argc, char **argv, Options *opt)
 			break;
 		case 'D':
 			if (optind == argc || !optarg)
-				opt->disc_str = (char *) "2";
+				*opt->disc_str = "2";
 			else
-				opt->disc_str = optarg;
+				*opt->disc_str = optarg;
 			break;
 		case 'P':
 			if (optind == argc || !optarg)
@@ -244,7 +240,7 @@ void parse_options (i32 argc, char **argv, Options *opt)
 				/* optional argument handling */
 				switch (prev_ch) {
 				case 'D':
-					opt->disc_str = argv[optind - 1];
+					*opt->disc_str = argv[optind - 1];
 					break;
 				case 'P':
 					opt->preload_lib = argv[optind - 1];

src/options.h
@@ -15,6 +15,7 @@
 #define OPTIONS_H
 
 #include <list>
+#include <string>
 
 /* local includes */
 #include <common.h>
@@ -41,8 +42,8 @@ public:
 	char	*cfg_path;
 	char	*pre_cmd;
 	char	*preload_lib;
-	char	*disc_str;
-	char	*disc_lib;
+	string	*disc_str;
+	string	*disc_lib;
 	bool	do_adapt;
 	bool	run_scanmem;
 	// no direct CLI input

src/ugtrain.cpp
@@ -840,9 +840,9 @@ static i32 prepare_dynmem (Options *opt, list<CfgEntry> *cfg,
 #endif
 
 	// Check for discovery first
-	if (opt->disc_str) {
+	if (!opt->disc_str->empty()) {
 		pos += snprintf(obuf + pos, sizeof(obuf) - pos, "%s",
-				opt->disc_str);
+				opt->disc_str->c_str());
 		if (pos + 2 > sizeof(obuf)) {
 			fprintf(stderr, "Buffer overflow\n");
 			goto err;
@@ -938,7 +938,7 @@ skip_memhack:
 		goto err;
 	if (setup_fifo(DYNMEM_OUT))
 		goto err;
-	if (opt->disc_str) {
+	if (!opt->disc_str->empty()) {
 		if (setup_fifo(MEMDISC_IN))
 			goto err;
 		*dfd = open(MEMDISC_IN, O_RDONLY | O_NONBLOCK);
@@ -1082,7 +1082,7 @@ i32 main (i32 argc, char **argv, char **env)
 	test_cfgparsing(opt);
 	ugout << "Found config for \"" << opt->proc_name << "\"." << endl;
 
-	if (opt->disc_str)
+	if (!opt->disc_str->empty())
 		allow_empty_cfg = init_discovery(opt);
 	else if (opt->run_scanmem)
 		allow_empty_cfg = init_scanmem(opt);
@@ -1141,7 +1141,7 @@ i32 main (i32 argc, char **argv, char **env)
 	}
 	ugout << "PID: " << pid << endl;
 
-	if (opt->disc_str) {
+	if (!opt->disc_str->empty()) {
 		process_discovery(opt, ifd, dfd, ofd, pid);
 		//list_regions(rlist);
 		ret = postproc_discovery(opt);
@@ -1153,7 +1153,7 @@ i32 main (i32 argc, char **argv, char **env)
 		return 0;
 	}
 
-	if (opt->do_adapt || opt->disc_str || opt->run_scanmem)
+	if (opt->do_adapt || !opt->disc_str->empty() || opt->run_scanmem)
 		return -1;
 
 	if (set_getch_nb(1) != 0)

commit 65673405fc57cd7db104b1bdda7a384731148463

discovery: Fix a segfault in pointer discovery
When using the pointer discovery e.g. for the Warzone2100 structure stats
pointer `ugtrain -D "p;368;0x6596fe;0x8;0x110;;2" warzone2100-64.conf`,
then ugtrain segfaults when overwriting the first character in the
discovery string (`p`) with the stage number (`2`) in process_discovery().
This has not been converted to C++ string handling, yet. So do it now.

Fixes: 45029cacbc2a ("ugtrain: Use C++ discovery strings and drop stage 5")

src/discovery.cpp
@@ -413,8 +413,10 @@ void process_discovery (Options *opt, i32 ifd, i32 dfd, i32 ofd, i32 pid)
 	struct list_head *rlist = opt->rlist;
 	pid_t worker_pid;
 
-	if (opt->disc_str->at(0) == 'p')
-		opt->disc_str[0] = opt->disc_str[opt->disc_offs];
+	if (opt->disc_str->at(0) == 'p') {
+		const char stage = opt->disc_str->c_str()[opt->disc_offs];
+		opt->disc_str->replace(0, 1, &stage);
+	}
 	if (opt->disc_str->at(0) == '0') {
 		if (opt->scanmem_pid > 0) {
 			wait_orphan(pid, opt->proc_name->c_str());