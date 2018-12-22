bcc901332af64ce18b28195527599616f77265c7
Sebastian Parschauer <s.parschauer@gmx.de>
2018-12-22 12:41:38 +0100

    examples: dangerdeep: Adapt to openSUSE Leap 15
    It is pretty annoying that the offsets within the memory objects
    change together with the increased size of the objects if comparing
    Leap 42.3 to Leap 15. Now also PIE is used. But okay, just adapt
    to the new build methods.

--- examples/adapt/dangerdeep_adapt64.sh
+++ examples/adapt/dangerdeep_adapt64.sh
@@ -5,8 +5,8 @@
 # We already know that dangerdeep is a 64-bit C++ application. Therefore,
 # objects are allocated most likely by the "_Znwm" function. This function
 # calls malloc internally.
-# And from previous discovery runs we already know the malloc size (1784 or
-# 0x6f8) for the Submarine object.
+# And from previous discovery runs we already know the malloc size for the
+# Submarine object.
 
 CWD=`dirname $0`
 cd "$CWD"
@@ -16,14 +16,20 @@ APP_PATHS="\
 /usr/games/dangerdeep
 "
 RC=0
-MSIZE="0x6f8"
+MSIZE1="0x6f8"
+MSIZE2="0x7f0"
 
 . ./_common_adapt.sh
 
 # check for changed paths
 get_app_path "$APP_PATH" "$APP_PATHS"
 
+MSIZE=$MSIZE1
 get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE," 5 17 17
+if [ $RC -ne 0 ]; then
+  MSIZE=$MSIZE2
+  get_malloc_code "$APP_PATH" "\<_Znwm@plt\>" "$MSIZE," 5 17 17
+fi
 if [ $RC -ne 0 ]; then exit 1; fi
 
 RESULT=`echo "1;${PATH_RESULT}Submarine;$MSIZE;0x$CODE_ADDR"`
