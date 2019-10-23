d8365400cffa6843c1bb4af190303730aa98046f
Sebastian Parschauer <s.parschauer@gmx.de>
2019-10-23 19:33:19 +0200

    dump: Optimize code structure
    There is quite some code duplication due to laziness and there are
    too high levels of indentation.
    So merge dump_ptr_mem() into dump_mem_obj() by adding an additional
    "prefix" parameter. Use NULL for no prefix. Then use De Morgan's
    transformation to get rid of a superfluous level of indentation in
    the loops.
    This makes the code much better readable. Functionality remains the
    same.

--- src/dump.cpp
+++ src/dump.cpp
@@ -1,6 +1,6 @@
 /* dump.cpp:    dump dynamic memory objects in process memory
  *
- * Copyright (c) 2012..2018 Sebastian Parschauer <s.parschauer@gmx.de>
+ * Copyright (c) 2012..2019 Sebastian Parschauer <s.parschauer@gmx.de>
  *
  * This file may be used subject to the terms and conditions of the
  * GNU General Public License Version 3, or any later version
@@ -23,8 +23,8 @@
 #include <dump.h>
 
 
-static void dump_ptr_mem (pid_t pid, i32 mfd, u32 obj_id, u32 ptr_id,
-			  ptr_t mem_addr, size_t size)
+static void dump_mem_obj (pid_t pid, i32 mfd, const char *prefix, u32 main_id,
+			  u32 obj_id, ptr_t mem_addr, size_t size)
 {
 	i32 fd, ret;
 	string fname, backup;
@@ -37,58 +37,9 @@ static void dump_ptr_mem (pid_t pid, i32 mfd, u32 obj_id, u32 ptr_id,
 		goto err_detach;
 	memdetach(pid);
 
-	fname += "p";
-	fname += to_string(ptr_id);
-	fname += "_";
-	if (obj_id < 100)
-		fname += "0";
-	if (obj_id < 10)
-		fname += "0";
-	fname += to_string(obj_id);
-	backup = fname;
-	backup += "~.dump";
-	fname += ".dump";
-
-	// store a backup if file exists for keeping two states to compare
-	fd = open(fname.c_str(), O_RDONLY);
-	if (fd >= 0) {
-		close(fd);
-		ret = rename(fname.c_str(), backup.c_str());
-		if (ret) {
-			ugerr << "Renaming " << fname << " to "
-			      << backup << " failed!" << endl;
-			goto err;
-		}
-	}
-	fd = open(fname.c_str(), O_CREAT | O_TRUNC | O_WRONLY, 0644);
-	if (fd < 0)
-		goto err;
-	wbytes = write(fd, buf, sizeof(buf));
-	if (wbytes < (ssize_t) sizeof(buf))
-		ugerr << fname << ": Write error!" << endl;
-	close(fd);
-	return;
-err_detach:
-	memdetach(pid);
-err:
-	return;
-}
-
-static void dump_mem_obj (pid_t pid, i32 mfd, u32 class_id, u32 obj_id,
-			  ptr_t mem_addr, size_t size)
-{
-	i32 fd, ret;
-	string fname, backup;
-	char buf[size];
-	ssize_t wbytes;
-
-	if (memattach(pid) != 0)
-		goto err;
-	if (memread((mfd >= 0) ? mfd : pid, mem_addr, buf, sizeof(buf)) != 0)
-		goto err_detach;
-	memdetach(pid);
-
-	fname = to_string(class_id);
+	if (prefix)
+		fname += prefix;
+	fname += to_string(main_id);
 	fname += "_";
 	if (obj_id < 100)
 		fname += "0";
@@ -156,35 +107,37 @@ void dump_all_mem_obj (pid_t pid, i32 mfd, list<CfgEntry> *cfg)
 	dump_maps(pid);
 
 	list_for_each (cfg, it) {
-		if (it->dynmem && it->dynmem != old_dynmem) {
-			obj_id = 0;
-			for (i = 0; i < it->dynmem->v_maddr.size(); i++) {
-				ugout << ">>> Dumping Class " << class_id
-				      << " Obj " << obj_id << " at 0x"
-				      << hex << it->dynmem->v_maddr[obj_id]
-				      << dec << endl;
-				dump_mem_obj(pid, mfd, class_id, obj_id,
-					     it->dynmem->v_maddr[obj_id],
-					     it->dynmem->mem_size);
-				obj_id++;
-			}
-			class_id++;
-			old_dynmem = it->dynmem;
+		if (!it->dynmem || it->dynmem == old_dynmem)
+			continue;
+		obj_id = 0;
+		for (i = 0; i < it->dynmem->v_maddr.size(); i++) {
+			ugout << ">>> Dumping Class " << class_id
+			      << " Obj " << obj_id << " at 0x"
+			      << hex << it->dynmem->v_maddr[obj_id]
+			      << dec << endl;
+			dump_mem_obj(pid, mfd, NULL, class_id, obj_id,
+				     it->dynmem->v_maddr[obj_id],
+				     it->dynmem->mem_size);
+			obj_id++;
 		}
+		class_id++;
+		old_dynmem = it->dynmem;
 	}
 
 	list_for_each (cfg, it) {
-		if (it->dynmem && it->ptrtgt) {
-			obj_id = 0;
-			for (i = 0; i < it->dynmem->v_maddr.size(); i++) {
-				if (it->v_oldval[obj_id].u64 > 0 &&
-				    it->ptrtgt->v_state[obj_id] >= PTR_SETTLED)
-					dump_ptr_mem(pid, mfd, obj_id, ptr_id,
-						     it->v_oldval[obj_id].ptr,
-						     it->ptrtgt->mem_size);
-				obj_id++;
-			}
-			ptr_id++;
+		if (!it->dynmem || !it->ptrtgt)
+			continue;
+		obj_id = 0;
+		for (i = 0; i < it->dynmem->v_maddr.size(); i++) {
+			if (it->v_oldval[obj_id].u64 == 0 ||
+			    it->ptrtgt->v_state[obj_id] < PTR_SETTLED)
+				goto skip_obj;
+			dump_mem_obj(pid, mfd, "p", ptr_id, obj_id,
+				     it->v_oldval[obj_id].ptr,
+				     it->ptrtgt->mem_size);
+skip_obj:
+			obj_id++;
 		}
+		ptr_id++;
 	}
 }
