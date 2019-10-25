ugtrain/ugtrain

commit bf81888fb841c60034f47a909a6a1cce4e8dfa29

cfg: Add statmem "dump" parsing and output
Static memory areas cannot be dumped right now but it makes sense
to compare two different states of a static memory area to easily
find further interesting values.

So introduce new config entry syntax for static memory only:
dump StartAddr Size [LibName]

Examples:
dump 0x620000 1000
dump 0x220000 1000
dump 0x220000 1000 libbt_objects.so

Put them into a list in the options, print that list at the end when
printing the config, clean up that list again.
Also fix a comment about the return value of parse_pic_lib().

Reference: #61

src/cfgentry.h
@@ -27,6 +27,13 @@
 
 #define PTR_MAX ULONG_MAX             // value to show that a cache is invalid
 
+class DumpEntry {
+public:
+	ptr_t addr;                   // static memory address
+	size_t mem_size;
+	string *lib;                  // for PIC support
+};
+
 class CacheEntry {
 public:
 	ptr_t offs;                   // offset within dynmem object

src/cfgoutput.cpp
@@ -17,6 +17,21 @@
 #include <cfgoutput.h>
 
 
+static void output_dump_entries (Options *opt)
+{
+	list<DumpEntry>::iterator dit;
+	if (opt->dump_list->empty())
+		return;
+	ugout << "statmem dump areas:" << endl;
+	list_for_each(opt->dump_list, dit) {
+		ugout << "  dump " << "0x" << hex << dit->addr << dec << " "
+		      << dit->mem_size;
+		if (dit->lib && !dit->lib->empty())
+			cout << " " << *dit->lib;
+		cout << endl;
+	}
+}
+
 static void output_val (struct type *type, value_t value, const char *suffix)
 {
 	if (type->is_float) {
@@ -311,4 +326,5 @@ skip_hl:
 		output_config_en(cfg_en, "");
 		output_checks(cfg_en);
 	}
+	output_dump_entries(opt);
 }

src/cfgparser.cpp
@@ -42,6 +42,7 @@ typedef enum {
 	NAME_DYNMEM_FILE,
 	NAME_PTRMEM_START,
 	NAME_PTRMEM_END,
+	NAME_DUMP,
 	NAME_ADP_SCRIPT,
 	NAME_ADP_REQ
 } name_e;
@@ -316,6 +317,8 @@ start:
 		*name_type = NAME_NO_PRELOAD;
 	} else if (ret == "define") {
 		*name_type = NAME_DEFINE;
+	} else if (ret == "dump") {
+		*name_type = NAME_DUMP;
 	} else {
 		map<string,string>::iterator it;
 
@@ -769,7 +772,7 @@ static void parse_key_bindings (string *line, u32 lnr, u32 *start,
 
 /*
  * returns the string at the end of a line
- * returns NULL if there is no string, it is "exe" or contains blanks
+ * returns empty string if there is no string, it is "exe" or contains blanks
  */
 static inline string parse_pic_lib (string *line, u32 *start)
 {
@@ -1193,6 +1196,16 @@ void read_config (Options *opt, vector<string> *cfg_lines)
 				&start, false, NULL);
 			tmp_str = line.substr(start, string::npos);
 			macros[key] = tmp_str;
+			break;
+
+		case NAME_DUMP:
+			if (in_dynmem || in_ptrmem)
+				cfg_parse_err(&line, lnr, start);
+			DumpEntry dump_en;
+			dump_en.addr = parse_address(NULL, NULL, NULL, &line, lnr, &start);
+			dump_en.mem_size = parse_u32_value(&line, lnr, &start);
+			dump_en.lib = new string(parse_pic_lib(&line, &start));
+			opt->dump_list->push_back(dump_en);
 			break;
 
 		default:

src/options.cpp
@@ -112,6 +112,7 @@ void cleanup_options (Options *opt)
 {
 	list<CacheEntry>::iterator cait;
 	list<LibEntry>::iterator lit;
+	list<DumpEntry>::iterator dit;
 
 	// Free cache lists first
 	if (!opt->lib_list)
@@ -139,8 +140,17 @@ skip_stack_cache:
 		delete[] cait->data;
 	opt->cache_list->clear();
 	delete opt->cache_list;
-
 skip_statmem_cache:
+	if (!opt->dump_list)
+		goto skip_dump_list;
+	list_for_each (opt->dump_list, dit) {
+		if (dit->lib)
+			delete dit->lib;
+	}
+	opt->dump_list->clear();
+	delete opt->dump_list;
+
+skip_dump_list:
 	if (opt->cfg_path)
 		delete opt->cfg_path;
 	if (opt->dynmem_file)
@@ -187,6 +197,7 @@ static void init_options (Options *opt)
 	opt->stack = new StackOpt;
 	opt->stack->cache_list = new list<CacheEntry>;
 	opt->lib_list = new list<LibEntry>;
+	opt->dump_list = new list<DumpEntry>;
 }
 
 /*

src/options.h
@@ -79,6 +79,7 @@ public:
 	list<CfgEntry*>		**cfgp_map;
 	list<CacheEntry>	*cache_list;	// caching static memory
 	list<LibEntry>		*lib_list;	// handling statmem in libraries
+	list<DumpEntry>		*dump_list;	// dump static memory areas
 	StackOpt		*stack;
 	// options for testing
 	TESTING_OPT_VARS

commit ce5dce472035b3558d1efeb7c749c4e68e868107

ugtrain: Add PIC/PIE handling to statmem dump
Fix the DumpEntry structure by renaming "lib" to "lib_name" and use
a LibEntry "lib" to contain the library information. Modify and make
use of the FIND_ADD_LIB_ENTRY() macro to fill it. Enhance
handle_statmem_pie() and handle_statmem_pic() to add the required
load addresses. Then set the is_loaded flag for libraries to prevent
doing that twice.
Only dump static memory areas for libs which are actually loaded.

Reference: #61
Fixes: bf81888fb841 ("cfg: Add statmem "dump" parsing and output")

src/aslr.h
@@ -71,8 +71,9 @@ get_regions (pid_t pid, struct list_head *rlist)
  * Handle PIE for static memory
  */
 static inline void
-handle_statmem_pie (Options *opt, list<CfgEntry> *cfg)
+handle_statmem_pie (Options *opt)
 {
+	list<CfgEntry> *cfg = opt->cfg;
 	list<CfgEntry>::iterator it;
 	list<CheckEntry> *chk_lp;
 	list<CheckEntry>::iterator chk_it;
@@ -105,6 +106,14 @@ checks:
 		}
 	}
 
+	// handle static memory dump areas
+	list<DumpEntry>::iterator dit;
+	list_for_each (opt->dump_list, dit) {
+		if (dit->lib)
+			continue;
+		dit->addr += code_offs;
+	}
+
 	// clear dirty flags again
 	list<CacheEntry>::iterator cait;
 	list_for_each (opt->cache_list, cait)
@@ -115,8 +124,9 @@ checks:
  * Handle PIC for static memory
  */
 static inline void
-handle_statmem_pic (Options *opt, list<CfgEntry> *cfg, bool late_pic)
+handle_statmem_pic (Options *opt, bool late_pic)
 {
+	list<CfgEntry> *cfg = opt->cfg;
 	list<CfgEntry>::iterator it;
 	list<CheckEntry> *chk_lp;
 	list<CheckEntry>::iterator chk_it;
@@ -150,6 +160,17 @@ checks:
 				chk_it->cache->offs += chk_it->type.lib->start;
 			}
 		}
+	}
+
+	// handle static memory dump areas
+	list<DumpEntry>::iterator dit;
+	list_for_each (opt->dump_list, dit) {
+		if (!dit->lib || !dit->lib->start || dit->lib->is_loaded)
+			continue;
+		dit->addr += dit->lib->start;
+		dit->lib->is_loaded = true;
+		// Cache handling might set is_loaded for the same lib.
+		// So better run before cache handling.
 	}
 
 	// clear dirty flags again

src/cfgentry.h
@@ -27,13 +27,6 @@
 
 #define PTR_MAX ULONG_MAX             // value to show that a cache is invalid
 
-class DumpEntry {
-public:
-	ptr_t addr;                   // static memory address
-	size_t mem_size;
-	string *lib;                  // for PIC support
-};
-
 class CacheEntry {
 public:
 	ptr_t offs;                   // offset within dynmem object
@@ -50,6 +43,14 @@ public:
 	// late PIC handling
 	bool is_loaded;   // for not changing offsets again
 	bool skip_val;    // for skipping 1 cycle value output
+};
+
+class DumpEntry {
+public:
+	ptr_t addr;                   // static memory address
+	size_t mem_size;
+	string *lib_name;             // for PIC support
+	LibEntry *lib;
 };
 
 class GrowEntry {

src/cfgoutput.cpp
@@ -26,8 +26,8 @@ static void output_dump_entries (Options *opt)
 	list_for_each(opt->dump_list, dit) {
 		ugout << "  dump " << "0x" << hex << dit->addr << dec << " "
 		      << dit->mem_size;
-		if (dit->lib && !dit->lib->empty())
-			cout << " " << *dit->lib;
+		if (dit->lib_name && !dit->lib_name->empty())
+			cout << " " << *dit->lib_name;
 		cout << endl;
 	}
 }

src/cfgparser.cpp
@@ -899,21 +899,21 @@ read_config_vect (string *path, char *home,
 	cfg_file.close();
 }
 
-#define FIND_ADD_LIB_ENTRY(entry)					\
+#define FIND_ADD_LIB_ENTRY(__entry)					\
 do {									\
 	list<LibEntry>::iterator lit;					\
 	bool found = false;						\
 	LibEntry lib_en;						\
 									\
 	list_for_each (opt->lib_list, lit) {				\
-		if (lit->name != *entry.type.lib_name)			\
+		if (lit->name != *__entry.lib_name)			\
 			continue;					\
-		entry.type.lib = &(*lit);				\
+		__entry.lib = &(*lit);					\
 		found = true;						\
 		break;							\
 	}								\
 	if (!found) {							\
-		lib_en.name = *entry.type.lib_name;			\
+		lib_en.name = *__entry.lib_name;			\
 		lib_en.start = 0;					\
 		lib_en.is_loaded = false;				\
 		lib_en.skip_val = false;				\
@@ -921,7 +921,7 @@ do {									\
 		lib_en.cache_list = new list<CacheEntry>;		\
 		lib_en.cache_list->push_back(cache_en);			\
 		opt->lib_list->push_back(lib_en);			\
-		entry.type.lib = &opt->lib_list->back();		\
+		__entry.lib = &opt->lib_list->back();			\
 	}								\
 } while (0)
 
@@ -1014,7 +1014,7 @@ void read_config (Options *opt, vector<string> *cfg_lines)
 			} else if (chk_en.type.lib_name && !chk_en.type.lib_name->empty()) {
 				if (in_dynmem || in_ptrmem)
 					cfg_parse_err(&line, lnr, start);
-				FIND_ADD_LIB_ENTRY(chk_en);
+				FIND_ADD_LIB_ENTRY(chk_en.type);
 			}
 			parse_data_type(&line, lnr, &start, &chk_en.type);
 			if (!chk_en.type.size)
@@ -1204,7 +1204,10 @@ void read_config (Options *opt, vector<string> *cfg_lines)
 			DumpEntry dump_en;
 			dump_en.addr = parse_address(NULL, NULL, NULL, &line, lnr, &start);
 			dump_en.mem_size = parse_u32_value(&line, lnr, &start);
-			dump_en.lib = new string(parse_pic_lib(&line, &start));
+			dump_en.lib_name = new string(parse_pic_lib(&line, &start));
+			dump_en.lib = NULL;
+			if (dump_en.lib_name && !dump_en.lib_name->empty())
+				FIND_ADD_LIB_ENTRY(dump_en);
 			opt->dump_list->push_back(dump_en);
 			break;
 
@@ -1225,7 +1228,7 @@ void read_config (Options *opt, vector<string> *cfg_lines)
 			} else if (cfg_en.type.lib_name && !cfg_en.type.lib_name->empty()) {
 				if (in_dynmem || in_ptrmem)
 					cfg_parse_err(&line, lnr, start);
-				FIND_ADD_LIB_ENTRY(cfg_en);
+				FIND_ADD_LIB_ENTRY(cfg_en.type);
 			}
 			parse_data_type(&line, lnr, &start, &cfg_en.type);
 			cfg_en.ptrtgt = NULL;

src/dump.cpp
@@ -170,11 +170,15 @@ skip_ptr:
 	main_id = 0;  // area id
 	obj_id = 0;
 	list_for_each (dump_list, dit) {
+		// Handle late PIC
+		if (dit->lib && !dit->lib->is_loaded)
+			goto skip_statmem;
 		ugout << ">>> Dumping Static Memory " << main_id
 		      << " Obj " << obj_id << " at 0x"
 		      << hex << dit->addr << dec << endl;
 		dump_mem_obj(pid, mfd, "s", main_id, obj_id,
 			     dit->addr, dit->mem_size);
+skip_statmem:
 		main_id++;
 	}
 }

src/options.cpp
@@ -144,8 +144,8 @@ skip_statmem_cache:
 	if (!opt->dump_list)
 		goto skip_dump_list;
 	list_for_each (opt->dump_list, dit) {
-		if (dit->lib)
-			delete dit->lib;
+		if (dit->lib_name)
+			delete dit->lib_name;
 	}
 	opt->dump_list->clear();
 	delete opt->dump_list;

src/ugtrain.cpp
@@ -1165,8 +1165,8 @@ i32 main (i32 argc, char **argv, char **env)
 	test_memattach(pid, &opt->procmem_fd);
 
 	handle_aslr(opt, cfg, ifd, ofd, pid, rlist);
-	handle_statmem_pie(opt, cfg);
-	handle_statmem_pic(opt, cfg, false);
+	handle_statmem_pie(opt);
+	handle_statmem_pic(opt, false);
 	handle_stack_end(opt, cfg, pid, rlist);
 
 	init_dmparams(dmparams, opt, cfg, ofd, pid, rlist);
@@ -1246,7 +1246,7 @@ i32 main (i32 argc, char **argv, char **env)
 				get_regions(pid, rlist);
 			regions_read = true;
 			find_lib_regions(rlist, opt);
-			handle_statmem_pic(opt, cfg, true);
+			handle_statmem_pic(opt, true);
 		}
 
 		if (!opt->pure_statmem) {